60a425d5125a4d4dcc6d10a5fa2d41b735b846e1
Nothbaum <m.nothbaum@uu.nl>
2025-02-20 07:43:33 +0100

    first mapper for title

--- app/Mappers/Datacite/Datacite4Mapper.php
+++ app/Mappers/Datacite/Datacite4Mapper.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Mappers\Datacite;
 
+use PHPUnit\Util\Json;
+use PhpParser\JsonDecoder;
+use App\Models\SourceDataset;
 use App\Mappers\MapperInterface;
 use App\Models\Ckan\DataPublication;
-use App\Models\SourceDataset;
 
 class Datacite4Mapper implements MapperInterface
 {
@@ -16,9 +18,112 @@ class Datacite4Mapper implements MapperInterface
         // read json text
         $metadata = json_decode($sourceDataset->source_dataset, true);
 
-        // set title
-        $dataset->title = $metadata['data']['attributes']['titles'][0]['title'];
+        // map title
+        $this->mapTitle($metadata, $dataset);
+
+        dd($dataset->title);
 
         return $dataset;
     }
+
+
+    public function mapTitle(array $metadata, DataPublication $dataset)
+    {
+        // check if present
+        // check for multiple occurences
+        // etc
+        // dd($metadata);
+        $titles = $metadata['data']['attributes']['titles'];
+        $titleSize = sizeof($titles);
+
+        if( $titleSize > 0) {
+
+            $titlesCandidates = [];
+
+            // main title is wihtout title type
+            // multiple without title type must have unique languages
+            // one title wihtout title type and no language and one that can have a language
+            // language can be empty
+
+            // check if there is more than one title present if not then enter more detailed selection
+            if($titleSize > 1 )
+            {
+                // filter the titles for the ones which dont have 'titleType', this is indicator for main title
+                foreach ($titles as $title) {
+                    if(!isset($title['titleType']) && isset($title['title']) ){
+                        array_push($titlesCandidates, $title);
+                    }
+                }
+
+
+                if(sizeof($titlesCandidates) == 1){
+                    // if only one is left then take that one
+                    $dataset->title = $titlesCandidates[0]['title'];
+                    return $dataset;
+
+                } else {
+
+
+                    // check if no "lang" property is set
+                    foreach ($titlesCandidates as $candidate) {
+
+
+                        if(!isset($candidate['lang'])){
+                            // check if there is an entry without a "lang"
+                            // if so then take it
+                            $dataset->title = $candidate['title'];
+                            return $dataset;
+
+                        } elseif ($candidate['lang'] == "") {
+                            // "lang" is set but empty
+                            // take it
+                            $dataset->title = $candidate['title'];
+                            return $dataset;
+
+                        }elseif (!str_contains($candidate['lang'], "en")){
+                            // lang does not contain "en"
+                            // remove it
+                            unset($titlesCandidates[array_search($candidate, $titlesCandidates)]);
+                        }
+
+                    }
+
+
+                    // now if only one candidate remains with "en" in "lang" take it
+                    if(sizeof($titlesCandidates) == 1){
+                        // if only one is left then take that one
+                        $dataset->title = $titlesCandidates[0]['title'];
+                        return $dataset;
+    
+                    } else {
+
+                        
+
+                    }
+
+                    dd($titlesCandidates);
+
+
+                }
+
+            }
+            else
+            {   
+                // the only title present
+                $dataset->title = $metadata['data']['attributes']['titles'][0]['title'];
+                return $dataset;
+
+            }
+
+        } else {
+            // exception handling? dotn fill out title
+            // then it should be stopped 
+            $dataset->title = "No title found";
+            return $dataset;
+
+        }
+
+        return $dataset;
+    }
+
 }
--- tests/Unit/Datacite4Test.php
+++ tests/Unit/Datacite4Test.php
@@ -23,14 +23,75 @@ class Datacite4Test extends TestCase
                     "attributes": {
                         "titles": [
                             {
+                                "lang": "en",
                                 "title": "Sedimentological and geochemical data of Lago di Vedana, north-eastern Italy"
+                            },
+                            {
+                                "lang": "en",
+                                "title": "Example Title"
+                            },
+                            {
+                                "lang": "es",
+                                "title": "Example Title spanish"
+                            },
+                            {   
+                                "lang": "es",
+                                "title": "Example Title no lang"
+                            },
+                            {
+                                "lang": "en",
+                                "title": "Example Subtitle",
+                                "titleType": "Subtitle"
+                            },
+                            {
+                                "lang": "fr",
+                                "title": "Example TranslatedTitle",
+                                "titleType": "TranslatedTitle"
+                            },
+                            {
+                                "lang": "en",
+                                "title": "Example AlternativeTitle",
+                                "titleType": "AlternativeTitle"
                             }
                         ]
                     }
                 }
             }';
         
+
+        // $sourceData->source_dataset = '
+        // {
+        //     "data": {
+        //         "id": "10.1594/pangaea.937090",
+        //         "type": "dois",
+        //         "attributes": {
+        //             "titles": [
+        //                 {
+        //                     "lang": "en",
+        //                     "title": "Example Title"
+        //                 },
+        //                 {
+        //                     "lang": "en",
+        //                     "title": "Example Subtitle",
+        //                     "titleType": "Subtitle"
+        //                 },
+        //                 {
+        //                     "lang": "fr",
+        //                     "title": "Example TranslatedTitle",
+        //                     "titleType": "TranslatedTitle"
+        //                 },
+        //                 {
+        //                     "lang": "en",
+        //                     "title": "Example AlternativeTitle",
+        //                     "titleType": "AlternativeTitle"
+        //                 }
+        //             ]
+        //         }
+        //     }
+        // }';
+
         $dataciteMapper = new Datacite4Mapper();
+        
         $dataset = $dataciteMapper->map($sourceData);
 
         $this->assertEquals($dataset->title, "Sedimentological and geochemical data of Lago di Vedana, north-eastern Italy");
