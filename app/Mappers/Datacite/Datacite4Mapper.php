a5bcbdc08dc37953a1090764dd3b7a48b69bd3c6
Nothbaum <m.nothbaum@uu.nl>
2025-02-24 16:23:42 +0100

    added intial logic and filtering for description

--- app/Mappers/Datacite/Datacite4Mapper.php
+++ app/Mappers/Datacite/Datacite4Mapper.php
@@ -24,6 +24,92 @@ class Datacite4Mapper implements MapperInterface
     }
 
     /**
+     * chooses one description from the datacite entry according to the following priorities
+     * if multiple descriptions are available:
+     * 
+     * 'descriptionType' not set
+     * no "lang" property set
+     * "lang" property set and empty
+     * 'lang' set to 'en'
+     * 'lang set to 'en-GB'
+     * if none apply take first in list
+     * 
+     * ASSUMPTIONS FOR PRIORITIES ARE COMMENTED IN THE FUNCTION
+     */
+    public function mapDescription(array $metadata, DataPublication $dataset){
+        $descriptions = $metadata['data']['attributes']['descriptions'];
+
+        if(sizeof($descriptions)>0){
+            $dataset->msl_description_abstract          = $this->receiveDescription('Abstract',            $descriptions);
+            $dataset->msl_description_methods           = $this->receiveDescription('Methods',             $descriptions);
+            $dataset->msl_description_series_information= $this->receiveDescription('SeriesInformation',   $descriptions);
+            $dataset->msl_description_table_of_contents = $this->receiveDescription('TableOfContents',     $descriptions);
+            $dataset->msl_description_technical_info    = $this->receiveDescription('TechnicalInfo',       $descriptions);
+            $dataset->msl_description_other             = $this->receiveDescription('Other',               $descriptions);
+        } else {
+            throw new MappingException('No description mapped (of any kind)');
+        }
+
+        return $dataset;
+    }
+
+    private function receiveDescription(string $descriptionType, array $descriptions): string{
+        
+        $descriptionString = '';
+        $descriptionsCandidates = [];
+
+        // filter if descriptions with descriptionType are present and collect candidates
+        foreach($descriptions as $description){
+            if(isset($description["descriptionType"]) && $description["descriptionType"] == $descriptionType){
+                array_push($descriptionsCandidates, $description);
+            }
+        }
+
+        if(sizeof($descriptionsCandidates) > 1 ) {
+            
+            // check if no "lang" property is set
+            // THIS ASSUMES THAT THERE CAN BE ONLY ONE ENTRY WITHOUT TITLE
+            foreach ($descriptionsCandidates as $candidate) {
+                if(!isset($candidate['lang'])){                            
+                    return $candidate['description'];
+                } 
+            }
+
+            // "lang" is set but empty
+            // THIS ASSUMES THAT THERE CAN BE ONLY ONE ENTRY WITH AN EMPTY LANG
+            foreach ($descriptionsCandidates as $candidate) {
+                    if ($candidate['lang'] == "") {
+                    return $candidate['description'];
+                }
+            }
+
+            // THIS ASSUMES THAT THERE CAN BE ONLY ONE ENTRY WITH 'en'
+            foreach ($descriptionsCandidates as $candidate) {
+                if($candidate['lang'] == "en"){
+                    return $candidate['description'];
+                }
+            }
+
+            // THIS ASSUMES THAT THERE CAN BE ONLY ONE ENTRY WITH 'en-GB'
+            foreach ($descriptionsCandidates as $candidate) {
+                if ($candidate['lang'] == "en-GB"){
+                    return $candidate['description'];
+                }
+            }
+
+            // nothing left. Just take the first one
+            return $descriptionsCandidates[0]['description'];
+
+        } 
+        elseif(sizeof($descriptionsCandidates) == 1) {   
+            return $descriptionsCandidates[0]['description'];
+        }
+
+        return $descriptionString;
+    }
+        
+
+    /**
      * chooses one title from the datacite entry according to the following priorities
      * if multiple titles are available:
      * 
