69ae49230253ff1851da05006e75e46359616c4b
Samsh001 <l.samshuijzen@uu.nl>
2025-03-27 08:59:47 +0000

    wip

--- app/Mappers/Datacite/DataciteMapper.php
+++ app/Mappers/Datacite/DataciteMapper.php
@@ -3,13 +3,16 @@ namespace App\Mappers\Datacite;
 
 use App\Mappers\MapperInterface;
 use App\Models\Ckan\DataPublication;
-use App\Models\SourceDataset;
 
 class DataciteMapper implements MapperInterface
 {
 
     public function map(array $metadata, DataPublication $dataPublication): DataPublication
     {
+        // select version based on metadata
+
+        // map aditional fields that independed of schema version
+
         $mapper = new Datacite4Mapper();
 
         return $mapper->map($metadata, $dataPublication);
