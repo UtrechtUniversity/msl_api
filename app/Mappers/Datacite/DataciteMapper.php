0f2b9f9145fde1e6e5d3ccd10e61dd38d49b149e
Samsh001 <l.samshuijzen@uu.nl>
2025-03-29 12:36:17 +0000

    set datapublication name in datacite mapper

--- app/Mappers/Datacite/DataciteMapper.php
+++ app/Mappers/Datacite/DataciteMapper.php
@@ -10,11 +10,20 @@ class DataciteMapper implements MapperInterface
     public function map(array $metadata, DataPublication $dataPublication): DataPublication
     {
         // select version based on metadata
+        $mapper = new Datacite4Mapper();
+
+        $dataset = $mapper->map($metadata, $dataPublication);
 
         // map aditional fields that independed of schema version
 
-        $mapper = new Datacite4Mapper();
+        // set data publication name
+        $dataPublication->name = $this->createDatasetNameFromDoi($dataPublication->msl_doi);
 
-        return $mapper->map($metadata, $dataPublication);
+        return $dataset;
+    }
+
+    private function createDatasetNameFromDoi(string $doi) 
+    {        
+        return md5($doi);
     }
 }
