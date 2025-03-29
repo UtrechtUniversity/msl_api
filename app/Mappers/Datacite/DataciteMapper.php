54e0923c558f12dc026177625e5193b826d1c2ed
Samsh001 <l.samshuijzen@uu.nl>
2025-03-29 13:21:08 +0000

    use specific datacitemapper version based on metadata

--- app/Mappers/Datacite/DataciteMapper.php
+++ app/Mappers/Datacite/DataciteMapper.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Mappers\Datacite;
 
+use App\Exceptions\MappingException;
 use App\Mappers\MapperInterface;
 use App\Models\Ckan\DataPublication;
 
@@ -10,12 +11,20 @@ class DataciteMapper implements MapperInterface
     public function map(array $metadata, DataPublication $dataPublication): DataPublication
     {
         // select version based on metadata
-        $mapper = new Datacite4Mapper();
+        switch ($metadata['metadataVersion'])
+        {
+            case 4:
+                $mapper = new Datacite4Mapper();
+                break;
+
+            default:
+                throw new MappingException('No DataCiteMapper found for version:' . $metadata['metadataVersion']);
+        }
 
         $dataset = $mapper->map($metadata, $dataPublication);
 
-        // map aditional fields that independed of schema version
-
+        // map aditional fields that are independed of the schema version
+        
         // set data publication name
         $dataPublication->name = $this->createDatasetNameFromDoi($dataPublication->msl_doi);
 
