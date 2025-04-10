ee059999331de64fd13d562923a0aed0e677429a
Samsh001 <l.samshuijzen@uu.nl>
2025-04-10 19:04:56 +0000

    cleanup KeywordHelper by adding function descriptions and removing replaced functions

--- app/Mappers/Helpers/KeywordHelper.php
+++ app/Mappers/Helpers/KeywordHelper.php
@@ -172,210 +172,13 @@ class KeywordHelper
 
         return $dataPublication;
     }
-    
-    
-    public function mapKeywords(BaseDataset $dataset, $keywords, $extractLastTerm = false, $lastTermDelimiter = '>')
-    {                
-        foreach ($keywords as $keyword) {
-            if($extractLastTerm) {
-                if(str_contains($keyword, $lastTermDelimiter)) {
-                    $splitKeywords = explode($lastTermDelimiter, $keyword);
-                    $keyword = end($splitKeywords);
-                }
-            }
-            
-            $keyword = trim($keyword);
-            $keywordTag = $this->cleanKeyword($keyword);
-            if(strlen($keywordTag) > 1) {
-                $dataset->tag_string[] = $keywordTag;
-                $dataset->addTag($keywordTag);                
-            }
-            
-            $searchKeywords = KeywordSearch::where('search_value', strtolower($keyword))->where('version', config('vocabularies.vocabularies_current_version'))->get();
-            
-            if(count($searchKeywords) > 0) {
-                foreach ($searchKeywords as $searchKeyword) {
-                    $keyword = $searchKeyword->keyword;
-                                       
-                    $dataset->addOriginalKeyword($keyword->value, $keyword->uri, $keyword->vocabulary->uri);
-                    $dataset->addUriToTag($keywordTag, $keyword->uri);
-                    
-                    foreach ($keyword->getFullHierarchy() as $enrichedKeyword) {
-                        if($enrichedKeyword->exclude_domain_mapping) {
-                            if($enrichedKeyword->uri == $keyword->uri) {
-                                $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], ['keyword']);
-                            } else {
-                                $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], ['parent'], [$keyword->uri]);
-                            }
-                        } else {
-                            if(isset($this->vocabularySubDomainMapping[$enrichedKeyword->vocabulary->name])) {
-                                $dataset->addSubDomain($this->vocabularySubDomainMapping[$enrichedKeyword->vocabulary->name], false);
-                                if($enrichedKeyword->uri == $keyword->uri) {
-                                    $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [$this->vocabularySubDomainMapping[$keyword->vocabulary->name]], ['keyword']);
-                                } else {
-                                    $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [$this->vocabularySubDomainMapping[$keyword->vocabulary->name]], ['parent'], [$keyword->uri]);
-                                }
-                            } else {
-                                if($enrichedKeyword->uri == $keyword->uri) {
-                                    $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], ['keyword']);
-                                } else {
-                                    $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], ['parent'], [$keyword->uri]);
-                                }
-                            }
-                            
-                        }
-                    }
-                    
-                }
-            }            
-            
-        }
-                        
-        return $dataset;
-    }
-    
-    public function mapKeywordsFromText(BaseDataset $dataset, $text, $source = "") 
-    {
-        $searchKeywords = KeywordSearch::where('exclude_abstract_mapping', false)->where('version', config('vocabularies.vocabularies_current_version'))->get();
-        
-        switch ($source) {
-            case 'title':
-                $dataset->msl_title_annotated = $dataset->title;
-                
-                $combinedMatches = [];
-                
-                foreach ($searchKeywords as $searchKeyword) {
-                    if($searchKeyword->search_value !== '') {
-                        $expr = $this->createKeywordSearchRegex($searchKeyword->search_value);
-                        if (preg_match($expr, $text)) {
-                            $keyword = $searchKeyword->keyword;
-                            
-                            //set keyword origin to parent if parent instead of source match
-                            
-                            foreach ($keyword->getFullHierarchy() as $enrichedKeyword) {
-                                $sourceRelation = $source;
-                                $childUri = [];
-                                if($enrichedKeyword->value !== $keyword->value) {
-                                    $sourceRelation = 'parent';
-                                    $childUri = [$keyword->uri];
-                                }
-                                                                                                    
-                                if($enrichedKeyword->exclude_domain_mapping) {
-                                    $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], [$sourceRelation], $childUri);
-                                } else {
-                                    if(isset($this->vocabularySubDomainMapping[$enrichedKeyword->vocabulary->name])) {
-                                        $dataset->addSubDomain($this->vocabularySubDomainMapping[$enrichedKeyword->vocabulary->name], false);
-                                        $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [$this->vocabularySubDomainMapping[$keyword->vocabulary->name]], [$sourceRelation], $childUri);
-                                    } else {
-                                        $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], [$sourceRelation], $childUri);
-                                    }                                    
-                                }                                
-                            }
-                                                        
-                            $matches = [];
-                            preg_match_all($expr, $dataset->msl_title_annotated, $matches, PREG_OFFSET_CAPTURE);
-                            
-                            foreach ($matches[0] as $match) {
-                                $combinedMatches[] = [
-                                    'uri' => [$keyword->uri],
-                                    'text' => $match[0],
-                                    'offset' => $match[1],
-                                    'end' => $match[1] + strlen($match[0])
-                                ];
-                            }
-                            
-                            
-                        }
-                                                
-                    }
-                }
-                // merge matches
-                $combinedMatches = $this->mergeMatches($combinedMatches);
-                
-                //remove elements included in greater elements (?)
-                $combinedMatches = $this->removeIncludedMatches($combinedMatches);
-                
-                //sort merge matches from start to end
-                usort($combinedMatches, function($a, $b) {
-                    return $a['offset'] <=> $b['offset'];
-                });
-                    
-                // annotate text
-                $annotatedText = $this->annotateText($dataset->msl_title_annotated, $combinedMatches);
-                
-                $dataset->msl_title_annotated = $annotatedText;
-                break;
-                
-            case 'notes':
-                $dataset->msl_notes_annotated = $dataset->notes;
-                
-                $combinedMatches = [];
-                
-                foreach ($searchKeywords as $searchKeyword) {
-                    if($searchKeyword->search_value !== '') {
-                        $expr = $this->createKeywordSearchRegex($searchKeyword->search_value);
-                        if (preg_match($expr, $text)) {
-                            $keyword = $searchKeyword->keyword;                                                       
-                            
-                            foreach ($keyword->getFullHierarchy() as $enrichedKeyword) {
-                                $sourceRelation = $source;
-                                $childUri = [];
-                                if($enrichedKeyword->value !== $keyword->value) {
-                                    $sourceRelation = 'parent';
-                                    $childUri = [$keyword->uri];
-                                }
-                                
-                                if($enrichedKeyword->exclude_domain_mapping) {
-                                    $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], [$sourceRelation], $childUri);
-                                } else {
-                                    if(isset($this->vocabularySubDomainMapping[$enrichedKeyword->vocabulary->name])) {
-                                        $dataset->addSubDomain($this->vocabularySubDomainMapping[$enrichedKeyword->vocabulary->name], false);
-                                        $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [$this->vocabularySubDomainMapping[$keyword->vocabulary->name]], [$sourceRelation], $childUri);
-                                    } else {
-                                        $dataset->addEnrichedKeyword($enrichedKeyword->value, $enrichedKeyword->uri, $enrichedKeyword->vocabulary->uri, [], [$sourceRelation], $childUri);
-                                    }
-                                    
-                                }
-                            }
-                            
-                            $matches = [];                           
-                            preg_match_all($expr, $dataset->msl_notes_annotated, $matches, PREG_OFFSET_CAPTURE);
-                            
-                            foreach ($matches[0] as $match) {
-                                $combinedMatches[] = [
-                                    'uri' => [$keyword->uri],
-                                    'text' => $match[0],
-                                    'offset' => $match[1],
-                                    'end' => $match[1] + strlen($match[0])
-                                ];
-                            }                                                       
-                        }                        
-                    }                                        
-                }
-                
-                // merge matches
-                $combinedMatches = $this->mergeMatches($combinedMatches);
-                
-                //remove elements included in greater elements (?)
-                $combinedMatches = $this->removeIncludedMatches($combinedMatches);
-                
-                //sort merge matches from start to end
-                usort($combinedMatches, function($a, $b) {
-                    return $a['offset'] <=> $b['offset'];
-                });
-                    
-                // annotate text
-                $annotatedText = $this->annotateText($dataset->msl_notes_annotated, $combinedMatches);
-                
-                $dataset->msl_notes_annotated = $annotatedText;
-                break;
-        }
-        
-                               
-        return $dataset;
-    }
-    
-    public function extractFromText($text, $domainVocabulariesOnly = false)
+               
+    /**
+     * Return array with keywords found within provided text
+     * @param string $text
+     * @param bool $domainVocabulariesOnly
+     */
+    public function extractFromText(string $text, $domainVocabulariesOnly = false): array
     {        
         if($domainVocabulariesOnly) {
             $vocabularies = Vocabulary::where('version', config('vocabularies.vocabularies_current_version'))->whereIn('name', ['rockphysics', 'analogue', 'paleomagnetism', 'geochemistry', 'microscopy', 'testbeds'])->get();
@@ -401,14 +204,23 @@ class KeywordHelper
         return $matchedKeywords;
     }
     
-    private function createKeywordSearchRegex($searchValue) {
+    /**
+     * Return regex string used to locate provided searchvalue
+     * @param string $searchValue
+     */
+    private function createKeywordSearchRegex(string $searchValue): string
+    {
         if(str_ends_with($searchValue, ',')) {
             return '/\b' . preg_quote($searchValue, '/') . '/i';
         }
         return '/\b' . preg_quote($searchValue, '/') . '\b/i';
     }
     
-    private function cleanKeyword($string)
+    /**
+     * cleanup provided text by removing non standard characters and maximizing lenght
+     * @param string $string
+     */
+    private function cleanKeyword(string $string): string
     {
         $keyword = preg_replace("/[^A-Za-z0-9 ]/", '', $string);
         if(strlen($keyword) >= 100) {
@@ -419,7 +231,12 @@ class KeywordHelper
         return trim($keyword);
     }
     
-    private function mergeMatches($matches) {
+    /**
+     * Merge array of matches by offsets
+     * @param array $matches
+     */
+    private function mergeMatches(array $matches): array
+    {
         $merged = [];
         
         foreach ($matches as $match) {
@@ -440,7 +257,12 @@ class KeywordHelper
         return $merged;
     }
     
-    private function removeIncludedMatches($matches) {
+    /**
+     * Remove matches that are included in other matches by provided offsets
+     * @param array $matches
+     */
+    private function removeIncludedMatches(array $matches): array
+    {
         $cleanedMatches = [];
         
         foreach ($matches as $matchValue) {
@@ -460,7 +282,13 @@ class KeywordHelper
         return $cleanedMatches;
     }
     
-    private function annotateText($text, $matches) {
+    /**
+     * Annotate provided text with matched location by adding html span elements
+     * @param string $text
+     * @param array $matches
+     */
+    private function annotateText(string $text, array $matches): string
+    {
         if(count($matches) > 0) {
             $offset = 0;
             foreach ($matches as $match) {
