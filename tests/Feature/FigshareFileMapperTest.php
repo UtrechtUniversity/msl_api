322f879cf240088362db15875c62bcce540307a1
Samsh001 <l.samshuijzen@uu.nl>
2025-04-11 17:54:50 +0000

    extend figsharefilemapper test

--- tests/Feature/FigshareFileMapperTest.php
+++ tests/Feature/FigshareFileMapperTest.php
@@ -25,8 +25,6 @@ class FigshareFileMapperTest extends TestCase
         $dataPublication = new DataPublication;
         $dataPublication->msl_doi = '12345';
 
-
-
         $this->mock('overload:' . FigshareFilesHelper::class, function(MockInterface $mock) {
             $mock->shouldReceive('getFileListByDOI')
                 ->once()
@@ -35,8 +33,22 @@ class FigshareFileMapperTest extends TestCase
                         'id' => 24044669,
                         'name' => "README.txt",
                         'size' => 638,
-                        'download_url' => "https://ndownloader.figshare.com/files/24044669"
-                    ]
+                        'is_link_only' => false,
+                        'download_url' => "https://ndownloader.figshare.com/files/24044669",
+                        'supplied_md5' => '3315787ee65dac6e49f9c67948e8c955',
+                        'computed_md5' => '3315787ee65dac6e49f9c67948e8c955',
+                        'mimetype' => 'text/plain'
+                    ],
+                    [
+                        'id' => 24044672,
+                        'name' => "data.zip",
+                        'size' => 4335,
+                        'is_link_only' => false,
+                        'download_url' => "https://ndownloader.figshare.com/files/24044672",
+                        'supplied_md5' => '57ca9aa3e468ef034059eba8efed90f3',
+                        'computed_md5' => '57ca9aa3e468ef034059eba8efed90f3',
+                        'mimetype' => 'application/zip'
+                    ],
                 ]);
         });
 
@@ -46,5 +58,9 @@ class FigshareFileMapperTest extends TestCase
         $this->assertEquals($dataPublication->msl_files[0]->msl_file_name, 'README.txt');
         $this->assertEquals($dataPublication->msl_files[0]->msl_download_link, 'https://ndownloader.figshare.com/files/24044669');
         $this->assertEquals($dataPublication->msl_files[0]->msl_extension, 'txt');
+
+        $this->assertEquals($dataPublication->msl_files[1]->msl_file_name, 'data.zip');
+        $this->assertEquals($dataPublication->msl_files[1]->msl_download_link, 'https://ndownloader.figshare.com/files/24044672');
+        $this->assertEquals($dataPublication->msl_files[1]->msl_extension, 'zip');
     }
 }
