d9b8f879eda4de4dc4f6973780a6f032b9d9eb9d
Nothbaum <m.nothbaum@uu.nl>
2025-06-10 11:34:35 +0200

    first draft of the page which redirects to the domain specific survey

---
+++ resources/views/frontend/contribute-select-scenario.blade.php
@@ -0,0 +1,57 @@
+@section('title', 'Scenario Survey')
+<x-layout_main>
+<div class="mainContentDiv flex-col">
+    <h1 class="pt-20">Data Tooling - Survey</h1>
+
+    <div class="max-w-screen-md pt-10 sm:pt-20">
+        <h3 class="pb-2 pt-6">What is this survey about?</h3>
+        <p class="inline">
+            We ask questions, you answer and everyone is happy!!
+        </p>
+    </div>
+
+    @php
+    $domainList = [
+      array(
+        "name"=>"Analogue modelling of geological processes", 
+        "routeParam" => 'analogue'
+      ),
+      array(
+        "name"=>"Geochemistry", 
+        "routeParam" => 'geochemistry'
+      ),
+      array(
+        "name"=>"Geo-energy test beds", 
+        "routeParam" => 'testbeds'
+      ),
+      array(
+        "name"=>"Rock and melt physics",  
+        "routeParam" => 'rockmelt'
+      ),
+      array(
+        "name"=>"Magnetism and paleomagnetism", 
+        "routeParam" => 'paleoMag'
+      ),
+      array(
+        "name"=>"Microscopy and tomography", 
+        "routeParam" => 'mircotomo'
+      ),
+    ]
+  @endphp
+
+        <h3 class="pb-2 pt-16">Select your domain!</h3>
+        <div class="sm:max-w-screen-lg flex md:flex-row flex-wrap sm:p-10 py-10 gap-8 justify-center place-items-center">
+            @foreach ($domainList as $infoElement)
+                    <a role="button" 
+                    href="{{ route('contribute-survey-scenario', ['domain' => $infoElement["routeParam"]]) }}"
+                    class="no-underline m-1 p-4 bg-base-300 rounded-lg w-64 h-24 place-content-center shadow-lg hover:bg-secondary-100 hover:text-secondary-900"
+                    > 
+                        <h5 class="text-base font-normal">{{ $infoElement["name"] }}</h5>
+                    </a>
+            @endforeach
+        </div>
+        
+</div>
+
+
+</x-layout_main>
