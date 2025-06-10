564cf977d978312aad4d6b6d899481ac6bdc7786
Nothbaum <m.nothbaum@uu.nl>
2025-06-10 11:33:47 +0200

    survey page empty

---
+++ resources/views/forms/contribute-survey-scenario.blade.php
@@ -0,0 +1,15 @@
+@section('title', 'Scenario Survey')
+<x-layout_main>
+<div class="mainContentDiv flex-col">
+    <h1 class="pt-20">Data Tooling - Survey</h1>
+
+        <p class="text-4xl p-20">
+            Survey to fill out
+        </p>
+
+        
+        
+</div>
+
+
+</x-layout_main>
