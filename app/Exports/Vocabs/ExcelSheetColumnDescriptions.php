44e12e23c652120af9f85485f02a3235f455dce7
Samsh001 <l.samshuijzen@uu.nl>
2025-06-26 17:59:35 +0000

    improve internal vocabulary excel export header descriptions

--- app/Exports/Vocabs/ExcelSheetColumnDescriptions.php
+++ app/Exports/Vocabs/ExcelSheetColumnDescriptions.php
@@ -12,55 +12,68 @@ class ExcelSheetColumnDescriptions implements FromArray, WithHeadings, WithMappi
     protected $descriptionsMapped = [
         [
             'columnName' => '1, 2, 3, etc.',
+            'columnPurpose' => 'For general use',
             'columnDescription' => 'Indicates the position of the term in the vocabulary taxonomy, where lower and higher numbers incidate broader and narrower terms, respectively.',
         ],
         [
             'columnName' => 'indicator terms',
+            'columnPurpose' => 'For general use',
             'columnDescription' => 'Words that are synonymous to the term, or serve to indicate that the term they are associated to is relevant to the dataset.',
         ],
         [
             'columnName' => 'uri',
+            'columnPurpose' => 'For general use',
             'columnDescription' => 'The unique identifier for the term',
         ],
         [
             'columnName' => 'hyperlink',
+            'columnPurpose' => 'For general use',
             'columnDescription' => 'The link to an external resource. This is not a definition, but may contain more information about the term. For instance a link to a page which provides more context',
         ],
         [
             'columnName' => 'external_uri',
+            'columnPurpose' => 'For general use',
             'columnDescription' => 'A reference to a term in an external vocabulary, which is equivalent to the term',
         ],
         [
             'columnName' => 'external_vocab_scheme',
+            'columnPurpose' => 'For general use',
             'columnDescription' => 'Name of the vocuabulary in which the column value "external_uri" exists. For example "GeoSciML"',
         ],
         [
             'columnName' => 'external_description',
+            'columnPurpose' => 'For general use',
             'columnDescription' => 'The description found on the "external_uri" of external vocabulary',
         ],
         [
             'columnName' => 'contributor_definition',
-            'columnDescription' => 'Definition or other notes about the term given by a contributor like you working in this Excel sheet. Structured information should be transferred to other columns, like "external_uri" or "hyperlink"',
+            'columnPurpose' => 'For continued vocabulary development',
+            'columnDescription' => '[may contain information that is not verified by the EPOS-MSL community]: Definition or other notes about the term, provided by a contributor, like yourself, when working with this excel sheet. Structured information should be transferred to other columns, like "external_uri" or "hyperlink"',
         ],
         [
             'columnName' => 'contributor_definition_link',
-            'columnDescription' => 'External links used by a contributor like you working in this Excel sheet. Structured information should be transferred to other columns, like "external_uri" or "hyperlink"',
+            'columnPurpose' => 'For continued vocabulary development',
+            'columnDescription' => '[may contain information that is not verified by the EPOS-MSL community]: External link provided by a contributor, like yourself, when working with this excel sheet. Structured information should be transferred to other columns, like "external_uri" or "hyperlink"',
         ],
         [
             'columnName' => 'exclude_domain_mapping',
+            'columnPurpose' => 'For use by MSL. Details how the term is used by EPOS-MSL',
             'columnDescription' => 'Indicates if the term is being used in mapping the associated data publication to one or more scientific domain(s) in MSL ("yes"/"no")',
         ],
         [
             'columnName' => 'terms_exclude_abstract_mapping',
-            'columnDescription' => 'Enter terms that should be excluded from being detected within the abstract, title, etc. during the importing process. Either the term itself (set in 1, 2, 3, etc.) or specific indicator terms (set in indicator terms) may be entered. Each term should start with a “#” and match the term specified in the other fields.',
+            'columnPurpose' => 'For use by MSL. Details how the term is used by EPOS-MSL',
+            'columnDescription' => 'Indicates if a term is excluded from being detected within the abstract, title, etc. during the importing process. Pertains either to the term itself (i.e. as included under column "1, 2, 3, etc."), or to specific indicator terms (i.e. as included under column "indicator terms"). Each term starts with a “#” and match the term specified in the other fields.',
         ],
         [
             'columnName' => 'selection_group_1',
-            'columnDescription' => 'Indicate if this term should be included in the group 1 words determining if data publications are relevant to MSL (“yes”/”no”). Relevant data publications have a combination of at least 1 group 1 and 1 group 2 word. Group 1 words indicate that an Earth scientific material or setting was studied.',
+            'columnPurpose' => 'For use by MSL. Details how the term is used by EPOS-MSL',
+            'columnDescription' => 'Indicates if this term should be included in the group 1 words determining if data publications are relevant to MSL (“yes”/”no”). Group 1 words indicates relevance to the Earth sciences, by matching to terms from the "material" or "geological setting" vocabularies used by EPOS-MSL. Relevant data publications have a combination of at least 1 group 1 and 1 group 2 word.',
         ],
         [
             'columnName' => 'selection_group_2',
-            'columnDescription' => 'Indicate if this term should be included in the group 2 words determining if data publications are relevant to MSL (“yes”/”no”). Relevant data publications have a combination of at least 1 group 1 and 1 group 2 word. Group 2 words indicate lab use, notably "apparatus", "measured property", etc.',
+            'columnPurpose' => 'For use by MSL. Details how the term is used by EPOS-MSL',
+            'columnDescription' => 'Indicates if this term should be included in the group 2 words determining if data publications are relevant to MSL (“yes”/”no”). Group 2 indicates relevance to Laboratory studies, by matching to terms used in e.g. "apparatus", "measured property", sections of domain-specific vocabularies used in EPOS-MSL. Relevant data publications have a combination of at least 1 group 1 and 1 group 2 word.',
         ],
     ];
 
@@ -71,7 +84,7 @@ class ExcelSheetColumnDescriptions implements FromArray, WithHeadings, WithMappi
 
     public function headings(): array
     {
-        return ['field', 'description'];
+        return ['field', 'purpose', 'description'];
     }
 
     public function title(): string
@@ -82,7 +95,9 @@ class ExcelSheetColumnDescriptions implements FromArray, WithHeadings, WithMappi
     public function map($row): array
     {
         return [
-            [$row['columnName'], $row['columnDescription']],
+            [
+                $row['columnName'], $row['columnPurpose'], $row['columnDescription']
+            ],
         ];
     }
 }
