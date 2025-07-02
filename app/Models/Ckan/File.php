7e5b7a41f3722089f013d5cdcf8adad66828a569
Samsh001 <l.samshuijzen@uu.nl>
2025-07-02 14:14:29 +0000

    add is_folder property to datapublication file

--- app/Models/Ckan/File.php
+++ app/Models/Ckan/File.php
@@ -9,13 +9,16 @@ class File implements CkanArrayInterface
 
     public string $msl_extension;
 
+    public string $msl_is_folder;
+
     public string $msl_timestamp;
 
-    public function __construct(string $fileName, string $downloadLink, string $extension, string $timestamp = "")
+    public function __construct(string $fileName, string $downloadLink, string $extension, bool $isFolder, string $timestamp = "")
     {
         $this->msl_file_name = $fileName;
         $this->msl_download_link = $downloadLink;
         $this->msl_extension = $extension;
+        $this->msl_is_folder = $isFolder;
         $this->msl_timestamp = $timestamp;
     }
 
