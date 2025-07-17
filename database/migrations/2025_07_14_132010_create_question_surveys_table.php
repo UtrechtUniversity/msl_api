c52182e2823cd20bcdb55353173c6b6f78ff5212
Nothbaum <m.nothbaum@uu.nl>
2025-07-17 16:50:38 +0200

    question_surveys migration

---
+++ database/migrations/2025_07_14_132010_create_question_surveys_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question_surveys', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('survey_id'); 
+            $table->unsignedBigInteger('question_id');
+            $table->Integer('order'); //how to save array data?
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_surveys');
+    }
+};
