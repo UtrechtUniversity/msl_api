696f1d71e5b5440ba418c52cc81a6ca166e94253
Nothbaum <m.nothbaum@uu.nl>
2025-07-17 16:54:04 +0200

    response model initial

---
+++ app/Models/Surveys/Response.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Surveys;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Response extends Model
+{
+    protected $fillable = [
+        'survey_id',
+        'email',
+    ];
+
+    public function survey() 
+    {
+        return $this->belongsTo(Survey::class);
+    }
+}
