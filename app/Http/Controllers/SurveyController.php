40106b52c12f550072f38117a6cf2fdf85506b47
Nothbaum <m.nothbaum@uu.nl>
2025-07-31 17:16:54 +0200

    survey controller

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+use EasyRdf\Literal\Integer;
+use Illuminate\Http\Request;
+use App\Models\Surveys\Answer;
+use App\Models\Surveys\Survey;
+use App\Models\Surveys\Response;
+use Illuminate\Http\RedirectResponse;
+use PhpOffice\PhpSpreadsheet\Calculation\Financial\CashFlow\Constant\Periodic\Interest;
+
+class SurveyController extends Controller
+{
+
+
+
+                /**
+     * Show the contribute survey scenario page
+     * 
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function contributeSurveyScenario($domain): View
+    {
+        $surveyId = 1;
+
+        return view('surveys.contribute-survey-scenario',[
+            'imageSource' => $domain,
+            'allQuestions' => $this->getSortedQuestions($surveyId),
+            'surveyId' => $surveyId
+        ]);
+    }
+
+                /**
+     * Show the contribute survey scenario page
+     * 
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function contributeSurveyScenarioProcess(Request $request, $surveyId): RedirectResponse
+    {
+        $sortedQuestions = $this->getSortedQuestions($surveyId);
+        
+        $validationFields = [];
+        
+        foreach ($sortedQuestions as $question) {
+            $validationFields[$question->question->sectionName] = $question->question->validation;
+        }
+
+        $request->validate($validationFields);
+        
+        $responseSurvey = Response::create([
+            'survey_id' => $surveyId,
+            'email' => $request->input('email')
+        ]);
+
+        // dd($request->input('PhaseSelect'));
+
+        foreach ($sortedQuestions as $question) {
+
+            if(is_array($request->input($question->question->sectionName))){
+                foreach ($request->input($question->question->sectionName) as $input) {
+                    Answer::create([
+                        'response_id' => $responseSurvey->id,
+                        'question_id' => $question->id,
+                        'answer' => $input,
+                    ]);
+                }
+             } else {
+                Answer::create([
+                    'response_id' => $responseSurvey->id,
+                    'question_id' => $question->id,
+                    'answer' => $request->input($question->question->sectionName),
+                ]);
+            }
+
+        }
+
+        // redirects to contribute-laboratory with the additonal elements located in components/notifications
+        return redirect('/')->with('modals', [
+            'type'      => 'success', 
+            'message'   => 'Thanks for your input!']
+        );
+    }
+
+
+    private function getSortedQuestions($surveyId){
+        $allQuestions = Survey::where('id', $surveyId)->first()->questions;
+        // the ->orderBy() has no effect for some reason
+        // this is why it is done this way
+        $allQuestionsSorted = [];
+        foreach ($allQuestions as $question) {
+            $allQuestionsSorted[$question->pivot->order] = $question;
+        }
+        return $allQuestionsSorted;
+    }
+}
