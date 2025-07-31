f0ba12f4de53950ef202535a1b23f5b40fb384e4
Nothbaum <m.nothbaum@uu.nl>
2025-07-31 16:53:37 +0200

    survey seeder test seed

--- database/seeders/SurveySeeder.php
+++ database/seeders/SurveySeeder.php
@@ -2,10 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\Surveys\Question;
-use App\Models\Surveys\QuestionType;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Surveys\Answer;
+use App\Models\Surveys\Survey;
 use Illuminate\Database\Seeder;
+use App\Models\Surveys\Question;
+use App\Models\Surveys\Response;
+use Illuminate\Support\Facades\DB;
+use App\Models\Surveys\QuestionType;
+use App\Models\Surveys\QuestionTypes\CheckBox;
+use App\Models\Surveys\QuestionTypes\RadioSelect;
+use App\Models\Surveys\QuestionTypes\TextQuestion;
+use App\Models\Surveys\QuestionTypes\SelectQuestion;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SurveySeeder extends Seeder
 {
@@ -15,38 +23,109 @@ class SurveySeeder extends Seeder
     public function run(): void
     {
         // dump current data
+        Survey::truncate();
         Question::truncate();
         QuestionType::truncate();
-
-        // Seed question type
+        Answer::truncate();
+        Response::truncate();
+        DB::table('question_survey')->truncate();
+       
+        // Seed question types
         $textQuestionType = QuestionType::create([
             'name' => 'text',
-            'class' => 'App\Models\Surveys\TextQuestion'
+            'class' => TextQuestion::class
         ]);
 
         $selectQuestionType = QuestionType::create([
             'name' => 'select',
-            'class' => 'App\Models\Surveys\SelectQuestion',
+            'class' => SelectQuestion::class,
         ]);
 
-        // questions
-        $question1 = Question::create([
-            'question_type_id' => $textQuestionType->id,
-            'question' => [
-                'label' => 'text label',
-            ]
+        $radioSelectType = QuestionType::create([
+            'name' => 'radio',
+            'class' => RadioSelect::class,
         ]);
 
+        $checkBoxType = QuestionType::create([
+            'name' => 'check',
+            'class' => CheckBox::class,
+        ]);
+
+        //survey test
+        $survey = Survey::create([
+            'name' => 'scenarioSurvey',
+            'active' => true,
+        ]);
+
+        $order = 0;
+        // seed test questions
+        $order++;
         $question2 = Question::create([
             'question_type_id' => $selectQuestionType->id,
             'question' => [
-                'label' => 'select label',
+                'title' => 'Is this a question?',
                 'options' => [
-                    '1' => 'Netherlands',
-                    '2' => 'Germany',
+                    'Netherlands',
+                    'Germany',
+                ],
+                'validation' => ['required'],
+                'sectionName' => "WhichCountry",
+                'placeholder' => 'Select an option from this list',
+                'titleBold' => true,
+
+            ]
+        ]);
+        $question2->surveys()->attach($survey->id, ['order' => $order]);
+
+        $order++;
+        $question1 = Question::create([
+            'question_type_id' => $textQuestionType->id,
+            'question' => [
+                'title' => 'Is this another question?',
+                'titleBold' => true,
+                'sectionName' => 'AnotherQuestion',
+                'textBlock' => true,
+                'placeholder' => 'Please type your answer here',
+                'validation' => ['required', 'min:50']
+            ]
+        ]);
+        $question1->surveys()->attach($survey->id, ['order' => $order]);
+
+        $order++;
+        $question3 = Question::create([
+            'question_type_id' => $radioSelectType->id,
+            'question' => [
+                'title' => 'Is this another question?',
+                'titleBold' => true,
+                'sectionName' => 'AgreementRadio',
+                'validation' => ['required'],
+                'options' => [
+                    'Agree very much',
+                    'Agree',
+                    'Disagree',
+                    'Disagree very much',
                 ],
             ]
         ]);
+        $question3->surveys()->attach($survey->id, ['order' => $order]);
+
+        $order++;
+        $question4 = Question::create([
+            'question_type_id' => $checkBoxType->id,
+            'question' => [
+                'title' => 'Is this another question?',
+                'titleBold' => true,
+                'sectionName' => 'PhaseSelect',
+                'validation' => ['required'],
+                'options' => [
+                    'Problem Identification',
+                    'Literature Review',
+                    'Setting Research Questions, Objectives, and Hypothesis',
+                    'Choosing the Design Study',
+                ],
+            ]
+        ]);
+        $question4->surveys()->attach($survey->id, ['order' => $order]);
 
     }
 }
