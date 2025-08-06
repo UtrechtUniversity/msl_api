19362e7843d88596025a7aca3208ed387c4a584a
Nothbaum <m.nothbaum@uu.nl>
2025-08-06 10:14:08 +0200

    image path and descriptions fetch+prep added

--- app/View/Components/SurveyComponent.php
+++ app/View/Components/SurveyComponent.php
@@ -6,17 +6,31 @@ use Closure;
 use App\Models\Surveys\Survey;
 use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class SurveyComponent extends Component
 {
 
     private $questionConfig;
+    private $allImages;
+    private $allDescriptions;
     /**
      * Create a new component instance.
      */
-    public function __construct($questionConfig)
+    public function __construct($questionConfig, $domain)
     {
         $this->questionConfig = $questionConfig;
+
+        foreach (File::files(public_path('images/surveys/scenario/'.$domain)) as $entry) {
+            if($entry->getExtension() == 'png'){
+                $this->allImages [] = 'images/surveys/scenario/'.$domain."/".$entry->getFilename();
+            } else if ($entry->getExtension() == 'json'){
+                $json = file_get_contents(public_path('images/surveys/scenario/'.$domain."/".$entry->getFilename()));
+                $this->allDescriptions = json_decode($json, true)['descriptions'];
+            }
+        }
+
     }
 
     /**
@@ -24,11 +38,10 @@ class SurveyComponent extends Component
      */
     public function render(): View|Closure|string
     {
-
-        // $allQuestions = Survey::where('id', $this->surveyID)->first()->questions;
-
         return view('components.survey-component',[
-            'questionConfig' => $this->questionConfig
+            'questionConfig' => $this->questionConfig,
+            'allImages' => $this->allImages,
+            'allDescriptions' => $this->allDescriptions
         ]);
     }
 }
