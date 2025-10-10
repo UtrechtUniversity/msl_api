c86dcb0ecb2f1373497a62effda7415d6d9a3ecf
Samsh001 <l.samshuijzen@uu.nl>
2025-10-10 07:25:30 +0000

    remove obsolete TNA mockup model use in old migration

--- database/migrations/2023_03_07_102118_create_tna_mockup_table.php
+++ database/migrations/2023_03_07_102118_create_tna_mockup_table.php
@@ -29,54 +29,7 @@ class CreateTnaMockupTable extends Migration
             $table->string('tnaStartDate');
             $table->string('tnaEndDate');
             $table->timestamps();
-        });
-        
-        
-        TnaMockup::create([
-            'organizationName' => '(UU) Universiteit Utrecht',
-            'facilityName' => 'EM Centre',
-            'facilityUrl' => 'https://www.uu.nl/en/organisation/faculty-of-science/collaboration/research-facilities/electron-microscopy',
-            'facilityCountry' => 'NL',
-            'facilityCity' => 'Utrecht',
-            'equipmentType' => 'Electron Microprobe',
-            'equipmentGroup' => 'EPMA (Electron Probe Micro Analyser)',
-            'equipmentName' => 'JXA-8530F Hyperprobe (EPMA)',
-            'equipmentUrl' => 'https://www.uu.nl/en/organisation/faculty-of-geosciences/collaboration/labs-and-facilities/electron-microscopy',
-            'equipmentManufacturer' => 'JEOL',
-            'tnaStartDate' => '01-05-2023',
-            'tnaEndDate' => '31-10-2023'
-        ]);
-        
-        TnaMockup::create([
-            'organizationName' => '(UU) Universiteit Utrecht',
-            'facilityName' => 'EM Centre',
-            'facilityUrl' => 'https://www.uu.nl/en/organisation/faculty-of-science/collaboration/research-facilities/electron-microscopy',
-            'facilityCountry' => 'NL',
-            'facilityCity' => 'Utrecht',
-            'equipmentType' => 'Electron Microscopy',
-            'equipmentGroup' => 'FIB-SEM (Focused Ion Beam - Scanning Electron Microscope)',
-            'equipmentName' => 'Helios Nanolab G3 (FIB-SEM)',
-            'equipmentUrl' => 'https://www.uu.nl/en/organisation/faculty-of-science/collaboration/research-facilities/electron-microscopy/advanced-electron-microscopes',
-            'equipmentManufacturer' => 'ThermoFisher Scientific',
-            'tnaStartDate' => '01-05-2023',
-            'tnaEndDate' => '31-10-2023'
-        ]);
-        
-        TnaMockup::create([
-            'organizationName' => '(INGV) Istituto Nazionale di Geofisica e Vulcanologia',
-            'facilityName' => 'INGV-RM1',
-            'facilityUrl' => 'https://www.ingv.it/it/monitoraggio-e-infrastrutture-per-la-ricerca/laboratori/laboratorio-hpht/electron-microscopy-laboratory',
-            'facilityCountry' => 'Italy',
-            'facilityCity' => 'Roma',
-            'equipmentType' => 'Electron Microprobe',
-            'equipmentGroup' => 'EPMA (Electron Probe Micro Analyser)',
-            'equipmentName' => 'JXA-8200 (EPMA)',
-            'equipmentUrl' => 'https://www.ingv.it/it/monitoraggio-e-infrastrutture-per-la-ricerca/laboratori/laboratorio-hpht/electron-microprobe-laboratory',
-            'equipmentManufacturer' => 'JEOL',
-            'tnaStartDate' => '01-05-2023',
-            'tnaEndDate' => '31-10-2023'
-        ]);
-        
+        });        
     }
 
     /**
