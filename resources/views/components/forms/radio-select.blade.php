e61c1f14d0ad9f4f0d915817096f2c73e6f48b3c
Nothbaum <m.nothbaum@uu.nl>
2025-10-23 11:36:52 +0200

    added vars for component radio select

--- resources/views/components/forms/radio-select.blade.php
+++ resources/views/components/forms/radio-select.blade.php
@@ -1,13 +1,7 @@
-{{-- 
-    vars
 
-    $ElementsArray => array(string)
-    $sectionName => string, describes the name of the elements for interactions
-
---}}
-
-<div>
-    @if (isset($title))
+<div class="w-full">
+    
+    @if (isset($title) && $title != '')
         <label for="{{ $sectionName  }}" 
             class="block mb-2 
             @if (isset($titleBold) && $titleBold)
@@ -18,27 +12,79 @@
         </label>
     @endif
     <div class="
-        form-control flex flex-col phone:flex-row w-full justify-center
-
-        ">   
+        form-control 
+        flex 
+        @if ($asCol)
+            flex-col
+        @else
+            flex-row 
+            sm:flex-nowrap
+            flex-wrap
+        @endif
+        
+        w-full 
+        justify-center
+        w-full
+        ">
         @foreach ($options as $key => $option)
+        <div class="
+            flex 
+            place-content-center
+            items-center
+            w-full">
+            @if ($infoIconsIds != [])
+                <x-ri-information-line id="{{ $infoIconsIds[$key] }}" class="info-icon mx-2"/>
+            @endif
+
             <label class="
-                label cursor-pointer flex flex-col gap-4 p-2 mx-2 phone:w-20 sm:w-28 border sm:border-0 
+                label cursor-pointer 
+                flex
+                w-full
+                @if ($asCol)
+                    flex-row
+                @else
+                    sm:flex-col
+                    flex-row
+                    sm:w-full
+                    max-w-96
+                @endif 
+                
+                gap-4 
+                p-2
+                justify-between
+                
+                hover-interactive
                 @if ($errors->has($sectionName))
                     error-highlight-input
                     rounded-xl
                 @endif
                 ">
-                <span class="label-text text-center" value={{ $key }}>{{ $option }}</span>
+                <span class="label-text text-primary-900 text-center" value={{ $key }}>{{ $option }}</span>
                 <input type="radio" 
                     value={{ $key }} 
-                    name={{ $sectionName }} 
-                    class="radio checked:bg-secondary-500 hover:bg-secondary-500" 
+
+                    @if ($sectionName != '')
+                        name={{ $sectionName.'[]' }}
+                    @endif
+
+                    @if ($ids != [])
+                        id={{ $ids[$key] }}
+                    @endif
+                    class="
+                    radio 
+                    checked:bg-secondary-500 hover:bg-secondary-500
+                    border
+                    border-secondary-500
+                    
+                    " 
                     @if (old($sectionName) == $key && old($sectionName) !== null)
                         checked
                     @endif
                     />
             </label>
+        </div>
+
+
         @endforeach
     </div>
     @if ($errors->has($sectionName))
