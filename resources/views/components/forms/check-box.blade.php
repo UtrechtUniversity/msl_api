8e40e29f56a24fb6c0bf1d676712fd474d557251
Nothbaum <m.nothbaum@uu.nl>
2025-10-23 11:34:35 +0200

    added vars for component checkbox

--- resources/views/components/forms/check-box.blade.php
+++ resources/views/components/forms/check-box.blade.php
@@ -1,7 +1,7 @@
 
 <div class="flex flex-col items-center w-full">
 
-    @if (isset($title))
+    @if ($title != '')
         <label for="{{ $sectionName  }}" 
             class="block mb-2 
             @if (isset($titleBold) && $titleBold)
@@ -14,13 +14,13 @@
         </label>
     @endif
 
-    <div class="flex flex-col w-96 px-10 py-8
+    <div class="flex flex-col w-full
       @if ($errors->has('multiCheckbox'))
               error-highlight bg-error-300 text-error-700 rounded-md
           @endif
       ">
       <div class="flex-col space-y-2 
-      place-content-center h-full
+      place-content-center h-full w-full
         @if ($errors->has($sectionName))
             error-highlight-input
             rounded-xl
@@ -28,22 +28,42 @@
       ">
   
       @foreach ( $options as $key => $option)
-          <div class="form-control">
-              <label class="cursor-pointer label p-2
-               hover:bg-secondary-100 hover:rounded-lg hover:text-secondary-900">
-                  <span class=" pr-4 text-sm" 
-                    value={{ $key }}
-                    name={{ $sectionName.'[]' }}
-                    >{{ $option }}</span>
-                  <input type="checkbox"
-                  value={{ $key }} 
-                  name={{ $sectionName.'[]' }}
-                  class="checkbox checkbox-secondary checkbox-md
+          <div class="form-control w-full">
+              <label class="
+                    w-full
+                    label p-2 
+                    text-secondary-900
+                    hover-interactive">
+                  <span 
+                    class="pr-4 text-sm w-full"
 
-                  " 
+                    value={{ $key }}
+
+                    @if ($sectionName != '')
+                        name={{ $sectionName.'[]' }}
+                    @endif
+                    >
+                        {{ $option }}
+                    </span>
+
+                  <input type="checkbox"
+
+                  value={{ $key }} 
+                  
+                  @if ($sectionName != '')
+                    name={{ $sectionName.'[]' }}
+                  @endif
+
+                  @if ($ids != [])
+                    id={{ $ids[$key] }}
+                  @endif
+
+                  class="checkbox checkbox-secondary checkbox-md rounded-sm border" 
+
                   @if (is_array(old( $sectionName )) && in_array($key, old( $sectionName )) )
                       checked="checked"
                   @endif
+
                   />
                   
               </label>
