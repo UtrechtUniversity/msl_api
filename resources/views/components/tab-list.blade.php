832e3d1183b5ab7764201c63a340c7627a084071
Nothbaum <m.nothbaum@uu.nl>
2025-10-27 16:47:50 +0100

    tablist added empty check

--- resources/views/components/tab-list.blade.php
+++ resources/views/components/tab-list.blade.php
@@ -13,25 +13,23 @@
 <div role="tablist" class="
 tabs 
 tabs-lift
-p-4 bg-primary-200 
+sm:p-4 bg-primary-200 
 text-sm
 sm:text-base
 whitespace-nowrap
 ">
 
     @foreach ( $allTabs as $description => $content)
-
-
-        <input type="radio" name="my_tabs_2" role="tab" class="tab hover-interactive p-2 px-4" 
+        @if ($content['content']  != '')
+            <input type="radio" name="my_tabs_2" role="tab" class="tab hover-interactive p-2 px-4" 
             aria-label="{{ $description }}" 
             @if ($content['id'] == $checkedElementId) checked='checked' @endif
-        />
-        <div role="tabpanel" 
-            class="tab-content tabs-div bg-primary-100 
-            whitespace-normal">
-            <p>{!! $content['content'] !!}</p>
-        </div>
-
+            />
+            <div role="tabpanel" 
+                class="tab-content tabs-div bg-primary-100 
+                whitespace-normal">
+                <p>{!! $content['content'] !!}</p>
+            </div>
+        @endif
     @endforeach
-
 </div>
