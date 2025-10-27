8b1f3b665b0ae76db69721b919e48953d0a064db
Nothbaum <m.nothbaum@uu.nl>
2025-10-27 16:45:55 +0100

    tab links comment removal

--- resources/views/components/tab-links.blade.php
+++ resources/views/components/tab-links.blade.php
@@ -19,23 +19,6 @@ includeIcon    = string - to select which icon
     
     <div role="tablist" class="tabs tabs-box tabs-md flex flex-row bg-primary-200 ">
 
-            
-        {{-- @if (isset($includeIcon))
-
-        
-            @foreach ($routes as $routeKey => $route)
-
-                    <a role="tab" href="{{ $route  }}" class="tab hover-interactive">
-                        @if ($includeIcon == 'goBack')
-                            <x-ri-arrow-left-line id="" class="goBack-icon"/>
-                        @endif                        
-                        {{ $routeKey }}                    
-                    </a>
-
-            @endforeach
-
-        @else --}}
-
             @foreach ($routes as $routeKey => $route)
                     @if (isset($routeActive) && $routeActive == $route)
                         <a role="tab" href="{{ $route }}" class="tab tab-active w-20 hover-interactive">{{ $routeKey }}</a>
@@ -43,9 +26,6 @@ includeIcon    = string - to select which icon
                         <a role="tab" href="{{ $route  }}" class="tab w-20 hover-interactive">{{ $routeKey }}</a>
                     @endif
             @endforeach
-
-        {{-- @endif --}}
-
 
     </div>
 </div>
