b0332784aa8fdb26058ee72c299dc1483bb155f4
Nothbaum <m.nothbaum@uu.nl>
2025-10-30 11:42:00 +0100

    review feedback added

--- resources/views/components/list-views/table-list.blade.php
+++ resources/views/components/list-views/table-list.blade.php
@@ -4,35 +4,36 @@
         'numericKeys' => false,
         'textSize' => 'sm'
     ])
-
-<table class="table-fixed w-full">
-    <tbody>
-        @foreach ($entries as $key => $value)
-            @if ($value != '')
-                <tr>
-                    @if ($withKeys)
-                        <td class=" w-20 sm:w-40 text-{{ $textSize }} p-0">
-                                @if (!$numericKeys)
-                                    {{ $key }}
-                                @else
-                                    @if (is_numeric($key))
+<div class="w-full py-2">
+    <table class="table-fixed w-full">
+        <tbody>
+            @foreach ($entries as $key => $value)
+                @if ($value != '')
+                    <tr>
+                        @if ($withKeys)
+                            <td class=" w-20 sm:w-40 text-{{ $textSize }} p-0">
+                                    @if (!$numericKeys)
                                         {{ $key }}
                                     @else
-                                        {{ '' }}
+                                        @if (is_numeric($key))
+                                            {{ $key }}
+                                        @else
+                                            {{ '' }}
+                                        @endif
                                     @endif
-                                @endif
-                        </td>
-                    @endif
-
-                    <td class="text-{{ $textSize }} p-0">
-                        @if (filter_var($value, FILTER_VALIDATE_URL))
-                            <a class='underline hover-interactive' href={{ $value }}>{{ $value }}</a>
-                        @else
-                            {{ $value }}
+                            </td>
                         @endif
-                    </td>
-                </tr>
-            @endif
-        @endforeach      
-    </tbody>
-</table>    
+    
+                        <td class="text-{{ $textSize }} p-0">
+                            @if (filter_var($value, FILTER_VALIDATE_URL))
+                                <a class='underline hover-interactive' href={{ $value }}>{{ $value }}</a>
+                            @else
+                                {{ $value }}
+                            @endif
+                        </td>
+                    </tr>
+                @endif
+            @endforeach      
+        </tbody>
+    </table>    
+</div>
