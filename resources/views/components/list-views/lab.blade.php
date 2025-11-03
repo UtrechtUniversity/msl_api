035223398bfa7ed7e44714083fa02c3a3a49f250
Nothbaum <m.nothbaum@uu.nl>
2025-11-03 09:35:56 +0100

    lab component list view simplified

--- resources/views/components/list-views/lab.blade.php
+++ resources/views/components/list-views/lab.blade.php
@@ -1,8 +1,21 @@
 {{-- 
     data    = (array)
 --}}
+<a class="self-center w-9/12 no-underline hover-interactive p-4" 
+    href="{{ route('lab-detail', ['id' => $data['name']]) }}">
+    @if (isset( $data['title']))
+        <h4 class="text-left">{{  $data['title'] }}</h4> 
+    @endif
 
-<a class="self-center w-9/12 no-underline" href="{{ route('lab-detail', ['id' => $data['name']]) }}">
+    @if (isset($data['msl_domain_name']))
+        <p>{{ $data['msl_domain_name'] }}</p>
+    @endif 
+
+    @if (isset($data['msl_organization_name']))
+        <p class="italic ">{{ $data['msl_organization_name'] }}</p>
+    @endif        
+</a>
+{{-- <a class="self-center w-9/12 no-underline" href="{{ route('lab-detail', ['id' => $data['name']]) }}">
 
 <div class="hover:bg-secondary-100 ">
 
@@ -23,4 +36,4 @@
 
 </div>    
 
-</a>
+</a> --}}
