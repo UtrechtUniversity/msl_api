f1c3fb3ab54d7f95b53af06f41dbf5ed1e1c4744
Nothbaum <m.nothbaum@uu.nl>
2025-11-03 11:48:50 +0100

    height fixing for equipment map view

--- resources/views/frontend/equipment-map.blade.php
+++ resources/views/frontend/equipment-map.blade.php
@@ -21,9 +21,9 @@
 
     <div class="main-content">
         <div class="sub-content-wide flex place-content-center w-full">
-            <div class="drawer lg:drawer-open ">
+            <div class="drawer lg:drawer-open h-170">
                 <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-                <div class="drawer-content bg-secondary-100 flex relative ">
+                <div class="drawer-content bg-secondary-100 flex relative h-170 ">
                     {{-- content here --}}
                     <div class="z-40 p-0 w-10 h-52 fixed inset-y-1/2 left-0 bg-secondary-200 lg:hidden  opacity-75 hover:opacity-100">
                         <label for="my-drawer-2" class="btn drawer-button w-full h-full flex flex-col justify-center z-40"
@@ -35,7 +35,7 @@
                           </p>
                         </label>
                     </div>
-                    <div class="w-full min-h-full bg-primary-100">
+                    <div class="w-full bg-primary-100">
 
                         {{-- list view --}}    
                         <div class="list-view">
@@ -46,15 +46,15 @@
                     </div>
 
                 </div>
-                <div class="drawer-side ">
+                <div class="drawer-side">
                     <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
                     {{-- side bar --}}
 
-                    <ul class="menu bg-primary-200 min-h-full p-0 w-80 text-primary-900">
+                    <ul class="menu bg-primary-200 p-0 w-80 text-primary-900 h-170 ">
                     <!-- Sidebar content here -->
-                    @include('components.search-div-filters',[
-                        'filterDataPath' => 'public/equipment.json'
-                    ])
+                        @include('components.search-div-filters',[
+                            'filterDataPath' => 'public/equipment.json'
+                        ])
                     </ul>
                 </div>
             </div>
