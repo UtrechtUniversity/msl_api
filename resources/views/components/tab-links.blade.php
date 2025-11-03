5cbd9500419f91b22eae21c559bf05c4268e9203
Nothbaum <m.nothbaum@uu.nl>
2025-11-03 13:31:09 +0100

    tab links component responsiveness when multiple of its kind are in same div

--- resources/views/components/tab-links.blade.php
+++ resources/views/components/tab-links.blade.php
@@ -17,13 +17,13 @@ includeIcon    = string - to select which icon
         <h5 class="pb-2">{{ $categoryName }}</h5>
     @endif
     
-    <div role="tablist" class="tabs tabs-box tabs-md flex flex-row bg-primary-200 ">
+    <div role="tablist" class="tabs tabs-box tabs-md flex flex-row bg-primary-200 justify-around">
 
             @foreach ($routes as $routeKey => $route)
                     @if (isset($routeActive) && $routeActive == $route)
-                        <a role="tab" href="{{ $route }}" class="tab tab-active w-20 hover-interactive !text-primary-900">{{ $routeKey }}</a>
+                        <a role="tab" href="{{ $route }}" class="tab tab-active w-full sm:w-20 hover-interactive !text-primary-900">{{ $routeKey }}</a>
                     @else
-                        <a role="tab" href="{{ $route  }}" class="tab w-20 hover-interactive !text-primary-800">{{ $routeKey }}</a>
+                        <a role="tab" href="{{ $route  }}" class="tab w-full sm:w-20 hover-interactive !text-primary-800">{{ $routeKey }}</a>
                     @endif
             @endforeach
 
