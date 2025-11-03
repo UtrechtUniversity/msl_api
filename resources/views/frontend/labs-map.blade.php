64377eba7cd315ef0911254710d1f5feda596ab2
Nothbaum <m.nothbaum@uu.nl>
2025-11-03 09:26:18 +0100

    labs map view update to new layout

--- resources/views/frontend/labs-map.blade.php
+++ resources/views/frontend/labs-map.blade.php
@@ -1,75 +1,62 @@
 @section('title', 'Laboratory map')
 <x-layout_main>
-
-    <div class="mainContentDiv">
-
-        {{-- a general no small width view notification --}}
-        @include('components.no_mobile_view', [
-            'breakpoint' => 'md'
+    <div class="tab-links-parent">
+        @include('components.tab-links',[
+            'categoryName'  => 'Laboratories',
+            'routes'        => array(
+                    'Map'   => route("labs-map"),
+                    'List'  => route("labs-list")
+            ),
+            'routeActive'   => route("labs-map")
         ])
+        @include('components.tab-links',[
+            'categoryName'  => 'Equipment',
+            'routes'        => array(
+                    'Map'   => route("equipment-map"),
+                    'List'  => route("equipment-list"),
+            ),
+        ])
+    </div>
 
-        {{-- top div --}}
-        <div class="noMobileView_wideScreenDiv">
+    <div class="main-content">
+        <div class="sub-content-wide flex place-content-center w-full">
+            <div class="drawer lg:drawer-open ">
+                <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+                <div class="drawer-content bg-secondary-100 flex relative ">
+                    {{-- content here --}}
+                    <div class="z-40 p-0 w-10 h-52 fixed inset-y-1/2 left-0 bg-secondary-200 lg:hidden  opacity-75 hover:opacity-100">
+                        <label for="my-drawer-2" class="btn drawer-button w-full h-full flex flex-col justify-center z-40"
+                        >
+                        <p 
+                        class=""
+                        style="writing-mode: sideways-lr;" >
+                            click here to see filters
+                          </p>
+                        </label>
+                    </div>
+                    <div class="w-full min-h-full bg-primary-100">
 
-
-            <div class="tabLinksParent">
-                @include('components.tab-links',[
-                    'categoryName'  => 'Laboratories',
-                    'routes'        => array(
-                            'Map'   => route("labs-map"),
-                            'List'  => route("labs-list")
-                    ),
-                    'routeActive'   => route("labs-map")
-                ])
-                @include('components.tab-links',[
-                    'categoryName'  => 'Equipment',
-                    'routes'        => array(
-                            'Map'   => route('equipment-map'),
-                            'List'  => route('equipment-list'),
-                    )
-
-                ])
-            </div>
-
-            {{-- content bottom div --}}
-            <div class="listMapDetailDivParent">
-
-
-                {{-- side bar --}}
-                @include('components.search-div-filters',[
-                    'filterDataPath' => 'public/laboratories.json'
-                    ])
-
-                {{-- main field --}}
-                <div class="listMapDiv">
-
-
-                    {{-- top search div --}}
-                    {{-- @include('components.search-div-list',[
-                        'searchFor'  => 'labs',
-                        'amountFound'   => 'labs' 
-                    ]) --}}
-
-                    {{-- list view --}}    
-                    <div class="listView">
-
-                        {{-- loop list content --}}
-                        @include('components.lab-map-view')
+                        {{-- list view --}}    
+                        <div class="list-view">
+                            @include('components.lab-map-view')    
+                        </div>
                         
                         
                     </div>
-                    
-                    {{-- bottom pagination of list --}}
-                    {{-- @include('components.pagination') --}}
-
 
                 </div>
+                <div class="drawer-side ">
+                    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+                    {{-- side bar --}}
 
-
+                    <ul class="menu bg-primary-200 min-h-full p-0 w-80 text-primary-900">
+                    <!-- Sidebar content here -->
+                    @include('components.search-div-filters',[
+                        'filterDataPath' => 'public/laboratories.json'
+                    ])
+                    </ul>
+                </div>
             </div>
-
         </div>
-
     </div>
-
 </x-layout_main>
