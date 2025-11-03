4414cfb8bf2655e98903e2d3964598f1e4bcfa98
Nothbaum <m.nothbaum@uu.nl>
2025-11-03 09:32:03 +0100

    equipment list view update to new layout

--- resources/views/frontend/equipment-list.blade.php
+++ resources/views/frontend/equipment-list.blade.php
@@ -1,78 +1,86 @@
 @section('title', 'Equipment list')
 <x-layout_main>
 
-    <div class="mainContentDiv">
-
-        {{-- a general no small width view notification --}}
-        @include('components.no_mobile_view', [
-            'breakpoint' => 'md'
+    <div class="tab-links-parent">
+        @include('components.tab-links',[
+            'categoryName'  => 'Laboratories',
+            'routes'        => array(
+                    'Map'   => route("labs-map"),
+                    'List'  => route("labs-list")
+            ),
         ])
+        @include('components.tab-links',[
+            'categoryName'  => 'Equipment',
+            'routes'        => array(
+                    'Map'   => route("equipment-map"),
+                    'List'  => route("equipment-list"),
+            ),
+            'routeActive'   => route("equipment-list")
+        ])
+    </div>
 
-        {{-- top div --}}
-        <div class="noMobileView_wideScreenDiv">
-
-
-            <div class="tabLinksParent">
-                @include('components.tab-links',[
-                    'categoryName'  => 'Laboratories',
-                    'routes'        => array(
-                            'Map'   => route("labs-map"),
-                            'List'  => route("labs-list")
-                    ),
-                ])
-                @include('components.tab-links',[
-                    'categoryName'  => 'Equipment',
-                    'routes'        => array(
-                            'Map'   => route("equipment-map"),
-                            'List'  => route("equipment-list"),
-                    ),
-                    'routeActive'   => route("equipment-list")
-                ])
-            </div>
-
-            {{-- content bottom div --}}
-            <div class="listMapDetailDivParent">
-
-
-                {{-- side bar --}}
-                @include('components.search-div-filters',['filterDataPath' => 'public/equipment.json'])
-
-                {{-- main field --}}
-                <div class="listMapDiv">
-
-
-                    {{-- top search div --}}
-                    @include('components.search-div-list',[
-                        'searchFor'     => 'equipment',
-                        'amountFound'   => 'pieces' 
-                    ])
-
-                    {{-- list view --}}    
-                    <div class="listView">
-
-                        {{-- loop list content --}}
-                        @foreach ($result->getResults() as $equipment)
-
-                            @include('components.list-views.equipment', [
-                                'data' => $equipment
-                            ])
-
-                        @endforeach         
-                        
-                        
+    <div class="main-content">
+        <div class="sub-content-wide flex place-content-center w-full">
+            <div class="drawer lg:drawer-open ">
+                <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+                <div class="drawer-content bg-secondary-100 flex relative ">
+                    {{-- content here --}}
+                    <div class="z-10 p-0 w-10 h-52 fixed inset-y-1/2 left-0 bg-secondary-200 lg:hidden  opacity-75 hover:opacity-100">
+                        <label for="my-drawer-2" class="btn drawer-button w-full h-full flex flex-col justify-center "
+                        >
+                        <p 
+                        class=""
+                        style="writing-mode: sideways-lr;" >
+                            click here to see filters
+                          </p>
+                        </label>
                     </div>
-                    
-                    {{-- bottom pagination of list --}}
-                    @include('components.pagination')
+                    <div class="w-full min-h-full bg-primary-100 pl-4">
+                        {{-- top search div --}}
+                        @include('components.search-div-list',[
+                            'searchFor'     => 'equipment',
+                            'amountFound'   => 'Pieces'
+                        ])
 
+                        {{-- list view --}}    
+                        <div class="list-view">
+                            @if (count($result->getResults()) > 0)
+                                @foreach ($result->getResults() as $equipment)
+                                    @include('components.list-views.equipment', [
+                                        'data' => $equipment
+                                    ])
+                                @endforeach     
+                            @else
+                            <h4 class="italic py-20">- no equipment pieces found -</h4>
+                            @endif
+                                         
+                        </div>
+                        
+
+                        <div class="list-view hidden">
+                            @include('components.pagination')
+                        </div>
+                        
+
+                    </div>
 
                 </div>
+                <div class="drawer-side ">
+                    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+                    {{-- side bar --}}
 
-
+                    <ul class="menu bg-primary-200 min-h-full p-0 w-80 text-primary-900">
+                    <!-- Sidebar content here -->
+                    @include('components.search-div-filters',[
+                        'filterDataPath' => 'public/equipment.json'
+                    ])
+                    </ul>
+                </div>
             </div>
-
         </div>
-
     </div>
+
+
+
 
 </x-layout_main>
