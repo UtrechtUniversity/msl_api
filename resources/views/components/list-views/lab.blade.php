54f7f18a743ff326cd3885e72f6e5b24d4e46c9e
Nothbaum <m.nothbaum@uu.nl>
2025-11-03 10:12:08 +0100

    comments removed

--- resources/views/components/list-views/lab.blade.php
+++ resources/views/components/list-views/lab.blade.php
@@ -1,6 +1,4 @@
-{{-- 
-    data    = (array)
---}}
+
 <a class="self-center w-9/12 no-underline hover-interactive p-4" 
     href="{{ route('lab-detail', ['id' => $data['name']]) }}">
     @if (isset( $data['title']))
@@ -15,25 +13,3 @@
         <p class="italic ">{{ $data['msl_organization_name'] }}</p>
     @endif        
 </a>
-{{-- <a class="self-center w-9/12 no-underline" href="{{ route('lab-detail', ['id' => $data['name']]) }}">
-
-<div class="hover:bg-secondary-100 ">
-
-    <div class="p-4"> 
-        
-        @if (isset( $data['title']))
-            <h4 class="text-left">{{  $data['title'] }}</h4> 
-        @endif
-
-        @if (isset($data['msl_domain_name']))
-            <p>{{ $data['msl_domain_name'] }}</p>
-        @endif 
-
-        @if (isset($data['msl_organization_name']))
-            <p class="italic ">{{ $data['msl_organization_name'] }}</p>
-        @endif        
-    </div>
-
-</div>    
-
-</a> --}}
