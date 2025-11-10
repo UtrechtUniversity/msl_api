e56306c7290b57798db3994b97eb84dd05c7cc0a
Nothbaum <m.nothbaum@uu.nl>
2025-11-10 10:56:05 +0100

    map default view in center of europe

--- resources/views/components/equipment-map-view.blade.php
+++ resources/views/components/equipment-map-view.blade.php
@@ -27,7 +27,7 @@
 
     var features = <?php echo json_encode($locations); ?>;        				
 
-    var map = L.map('map').setView([51.505, -0.09], 4);
+    var map = L.map('map').setView([53.505, 29.09], 4);
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
