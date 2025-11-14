a1aead0b21debcb6a94f732d4db7c3ef14001a3d
Samsh001 <l.samshuijzen@uu.nl>
2025-11-14 14:47:53 +0000

    use parts of  newer version of data-access

--- resources/views/components/list-views/data-publication.blade.php
+++ resources/views/components/list-views/data-publication.blade.php
@@ -2,44 +2,39 @@
     data    = (array)
 --}}
 
-<a class="self-center w-9/12 no-underline" href="{{ route('data-publication-detail', ['id' => $data['id']]) }}">
+<a class="self-center w-9/12 no-underline" href="{{ route('data-publication-detail', ['id' => $data->name]) }}">
+    <div class="hover:bg-secondary-100 ">
+        <div class="p-4">         
+            @if ($data->title != '')
+                <h4 class="text-left">{{  $data->title }}</h4> 
+            @else
+                <h4 class="text-left italic">- no title found -</h4> 
+            @endif
 
-<div class="hover:bg-secondary-100 ">
+            @if (sizeof($data->msl_creators) > 0)
+                <h5 class="text-left font-medium pt-4">
+                    @foreach ( $data->msl_creators as $authorKey => $author )
+                        {{ $author->getFullName() }} 
+                            @if (sizeof($data->msl_creators) -1 != $authorKey )
+                                |
+                            @endif
+                    @endforeach
+                </h5>
+            @else
+                <h5 class="text-left font-medium pt-4">- no authors found -</h5>
+            @endif
 
+            @if ($data->msl_publication_year != '')
+                <p>{{ $data->msl_publication_year }}</p>
+            @else
+                <p>- no publication year found -</p>
+            @endif
 
-    <div class="p-4"> 
-        
-        @if (isset( $data['title']))
-            <h4 class="text-left">{{  $data['title'] }}</h4> 
-        @endif
-
-        @if (isset($data['msl_authors']))
-            <h5 class="text-left font-medium pt-4">
-                @foreach ( $data['msl_authors'] as $authorKey => $author )
-                    {{ $author["msl_author_name"] }} 
-                    {{-- a little divider between names --}}
-                    @if (sizeof($data['msl_authors']) -1 != $authorKey )
-                        |
-                    @endif
-                @endforeach
-            </h5>
-        @endif
-
-        @if (isset($data['msl_publication_year']))
-            <p>{{ $data['msl_publication_year'] }}</p>
-        @endif
-
-
-        @if (isset($data['notes']))
-            {{-- https://laravel.com/docs/11.x/strings#method-str-limit --}}
-            <p class="italic ">{{ Str::limit($data['notes'], 295, preserveWords: true) }}</p>
-        @endif
-
-        
-
+            @if ($data->getMainDescription() != '')
+                <p class="italic ">{{ Str::limit($data->getMainDescription(), 295, preserveWords: true) }}</p>
+            @else
+                <p class="italic ">- no description found -</p>
+            @endif
+        </div>
     </div>
-
-
-</div>    
-
 </a>
