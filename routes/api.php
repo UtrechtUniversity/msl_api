16c53f5bb6a6008e9c7ad55c34708f363c02723e
d.nakou@uu.nl <d.nakou@uu.nl>
2025-11-21 11:26:21 +0100

    add prefixes

--- routes/api.php
+++ routes/api.php
@@ -35,31 +35,36 @@ Route::get('/vocabularies/term', [V1Controller::class, 'term']);
 Route::get('/facilities', [V1Controller::class, 'facilities']);
 
 Route::prefix('v1')->group(function () {
-    Route::get('/datapublications/rock_physics', [V1Controller::class, 'rockPhysics']);
-    Route::get('/datapublications/analogue', [V1Controller::class, 'analogue']);
-    Route::get('/datapublications/paleo', [V1Controller::class, 'paleo']);
-    Route::get('/datapublications/microscopy', [V1Controller::class, 'microscopy']);
-    Route::get('/datapublications/geochemistry', [V1Controller::class, 'geochemistry']);
-    Route::get('/datapublications/geoenergy', [V1Controller::class, 'geoenergy']);
-    Route::get('/datapublications/all', [V1Controller::class, 'all']);
+    Route::prefix('datapublications')->group(function () {
+        Route::get('/rock_physics', [V1Controller::class, 'rockPhysics']);
+        Route::get('/analogue', [V1Controller::class, 'analogue']);
+        Route::get('/paleo', [V1Controller::class, 'paleo']);
+        Route::get('/microscopy', [V1Controller::class, 'microscopy']);
+        Route::get('/geochemistry', [V1Controller::class, 'geochemistry']);
+        Route::get('/geoenergy', [V1Controller::class, 'geoenergy']);
+        Route::get('/all', [V1Controller::class, 'all']);
+    });
     Route::get('/vocabularies/term', [V1Controller::class, 'term']);
     Route::get('/facilities/all', [V1Controller::class, 'facilities']);
 });
 
 Route::prefix('v2')->group(function () {
-    Route::get('/datapublications/rock_physics', [V2DataPublicationController::class, 'rockPhysics']);
-    Route::get('/datapublications/analogue', [V2DataPublicationController::class, 'analogue']);
-    Route::get('/datapublications/paleo', [V2DataPublicationController::class, 'paleo']);
-    Route::get('/datapublications/microscopy', [V2DataPublicationController::class, 'microscopy']);
-    Route::get('/datapublications/geochemistry', [V2DataPublicationController::class, 'geochemistry']);
-    Route::get('/datapublications/geoenergy', [V2DataPublicationController::class, 'geoenergy']);
-    Route::get('/datapublications/all', [V2DataPublicationController::class, 'all']);
-
-    Route::get('/facilities/rock_physics', [V2FacilityController::class, 'rockPhysics']);
-    Route::get('/facilities/analogue', [V2FacilityController::class, 'analogue']);
-    Route::get('/facilities/paleo', [V2FacilityController::class, 'paleo']);
-    Route::get('/facilities/microscopy', [V2FacilityController::class, 'microscopy']);
-    Route::get('/facilities/geochemistry', [V2FacilityController::class, 'geochemistry']);
-    Route::get('/facilities/geoenergy', [V2FacilityController::class, 'geoenergy']);
-    Route::get('/facilities/all', [V2FacilityController::class, 'all']);
+    Route::prefix('datapublications')->group(function () {
+        Route::get('/rock_physics', [V2DataPublicationController::class, 'rockPhysics']);
+        Route::get('/analogue', [V2DataPublicationController::class, 'analogue']);
+        Route::get('/paleo', [V2DataPublicationController::class, 'paleo']);
+        Route::get('/microscopy', [V2DataPublicationController::class, 'microscopy']);
+        Route::get('/geochemistry', [V2DataPublicationController::class, 'geochemistry']);
+        Route::get('/geoenergy', [V2DataPublicationController::class, 'geoenergy']);
+        Route::get('/all', [V2DataPublicationController::class, 'all']);
+    });
+    Route::prefix('facilities')->group(function () {
+        Route::get('/rock_physics', [V2FacilityController::class, 'rockPhysics']);
+        Route::get('/analogue', [V2FacilityController::class, 'analogue']);
+        Route::get('/paleo', [V2FacilityController::class, 'paleo']);
+        Route::get('/microscopy', [V2FacilityController::class, 'microscopy']);
+        Route::get('/geochemistry', [V2FacilityController::class, 'geochemistry']);
+        Route::get('/geoenergy', [V2FacilityController::class, 'geoenergy']);
+        Route::get('/all', [V2FacilityController::class, 'all']);
+    });
 });
