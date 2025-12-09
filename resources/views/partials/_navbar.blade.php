5e975768d292753e76675c7e3047f1f3f7837322
Nothbaum <m.nothbaum@uu.nl>
2025-12-09 09:20:35 +0100

    small screen menu main button colour

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -41,13 +41,13 @@
             {{-- min nav --}}
             <div class="dropdown dropdown-hover relative px-4">
 
-                <div tabindex="0" role="button" class="btn btn-ghost min-[810px]:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
+                <div tabindex="0" role="button" class="btn btn-ghost min-[810px]:hidden hover-interactive">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 " fill="none" viewBox="0 0 24 24"
+                        stroke="#160f04">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h8m-8 6h16" />
                     </svg>
-                    <h3 class="align-top">Menu</h3>
+                    <h3 class="align-top text-primary-900">Menu</h3>
                 </div>
 
                 <ul tabindex="0"
