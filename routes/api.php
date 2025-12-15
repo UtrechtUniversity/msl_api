c121b292772cd6bc95220c8890a3b4c6d482da7d
d.nakou@uu.nl <d.nakou@uu.nl>
2025-12-15 15:28:27 +0100

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -57,7 +57,7 @@ Route::prefix('v2')->group(function () {
         Route::get('/microscopy', [V2DataPublicationController::class, 'microscopy']);
         Route::get('/geochemistry', [V2DataPublicationController::class, 'geochemistry']);
         Route::get('/geoenergy', [V2DataPublicationController::class, 'geoenergy']);
-        Route::get('/all', [V2DataPublicationController::class, 'all'])->name('v2-dp-all');
+        Route::get('/all', [V2DataPublicationController::class, 'all']);
     });
     Route::prefix('facilities')->group(function () {
         Route::get('/rock_physics', [V2FacilityController::class, 'rockPhysics']);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
-// import react from '@vitejs/plugin-react';
-// import vue from '@vitejs/plugin-vue';
 
-// import tailwindcss from 'tailwindcss'
 import commonjs from 'vite-plugin-commonjs';
 export default defineConfig({
     plugins: [
@@ -35,5 +32,4 @@ export default defineConfig({
         },
 
     },
-
 });
