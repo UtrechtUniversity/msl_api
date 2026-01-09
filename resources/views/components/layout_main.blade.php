c7c438253719164deee17763b9a2d7911576dbf1
Samsh001 <l.samshuijzen@uu.nl>
2026-01-09 09:46:56 +0000

    test tracking with changes from default js to work from local setup

--- resources/views/components/layout_main.blade.php
+++ resources/views/components/layout_main.blade.php
@@ -8,6 +8,23 @@
     @vite(['resources/css/app.css', 'resources/ts/app.ts', 'resources/css/treejs.css', 'resources/css/treejs-custom.css'])
     <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js"></script>
     <script src="https://unpkg.com/tippy.js@6/dist/tippy-bundle.umd.js"></script>
+    
+    <!-- Matomo -->
+    <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//acc.webstats.its.uu.nl/";
+        _paq.push(['setTrackerUrl', 'https://acc.webstats.its.uu.nl/'+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+    </script>
+    <!-- End Matomo Code -->
+
     <title>{{ config('app.name') }} - @yield('title')</title>
 </head>
 
