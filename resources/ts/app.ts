68e658bdbeb2be2bb46dac47b2beb04f3752b1d1
d.nakou@uu.nl <d.nakou@uu.nl>
2026-01-20 10:43:34 +0100

    Fix a window type

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 window.$ = $;
 //  Import Leaflet and CSS
-import L from 'leaflet';
+import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
