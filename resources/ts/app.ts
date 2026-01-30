6f20e1913bacb9df67711885c6c0a90088a61e51
Samsh001 <l.samshuijzen@uu.nl>
2026-01-30 11:09:48 +0100

    adjust leaflet import

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,25 +1,24 @@
 import $ from 'jquery';
 window.$ = $;
 //  Import Leaflet and CSS
-import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
+import * as L from 'leaflet';
 //  Import MarkerCluster AFTER exposing window.L
-window.L = L;
 import 'leaflet.markercluster';
-
 
 import markerIconUrl from "leaflet/dist/images/marker-icon.png";
 import markerIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
 
 // Override the default Leaflet icon  so that markers are working as expected
-
 L.Icon.Default.prototype.options.iconUrl = markerIconUrl;
 L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl;
 L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
 L.Icon.Default.imagePath = "";
 
 import '@fortawesome/fontawesome-free/css/all.css';
+export default L
