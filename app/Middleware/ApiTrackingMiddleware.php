30abf3a21e6b198ec227d1494858f2a58ad63186
Samsh001 <l.samshuijzen@uu.nl>
2026-02-02 09:42:24 +0100

    rename page tracking job

--- app/Middleware/ApiTrackingMiddleware.php
+++ app/Middleware/ApiTrackingMiddleware.php
@@ -2,7 +2,7 @@
 
 namespace App\Middleware;
 
-use App\Jobs\ProcessMatomoTrackingJob;
+use App\Jobs\ProcessMatomoPageTrackingJob;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
@@ -12,7 +12,7 @@ class ApiTrackingMiddleware
     public function handle(Request $request, Closure $next)
     {
         if(Config::boolean('matomo.enabled')) {
-            ProcessMatomoTrackingJob::dispatch($request->getClientIp(), $request->getRequestUri());
+            ProcessMatomoPageTrackingJob::dispatch($request->getClientIp(), $request->getRequestUri());
         }
 
         return $next($request);
