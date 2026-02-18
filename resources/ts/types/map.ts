f0925168906cd6e3b1242d4f7ff2b2f5ae963037
d.nakou@uu.nl <d.nakou@uu.nl>
2026-02-18 14:10:54 +0000

    move types

--- resources/ts/types/map.ts
+++ resources/ts/types/map.ts
@@ -6,18 +6,9 @@ export type Inclusive = typeof INCLUSIVE
 
 export type InclusiveOrExclusive = Exclusive | Inclusive
 
-export const TAB_CONFIG =
-    {
-        [EXCLUSIVE]: { label: 'Exclusive results', active: true },
-        [INCLUSIVE]: { label: 'Inclusive results', active: false }
-    } as const
 
 
 export type MappingOnTabs<T> = {
     [EXCLUSIVE]: T,
     [INCLUSIVE]: T
-}
-
-export function getMappingOnTabsObj<T>(t: T): MappingOnTabs<T> {
-    return { [EXCLUSIVE]: t, [INCLUSIVE]: t }
 }
