3e2c58895c796eac830fb95bd2fb8a095a733e27
d.nakou@uu.nl <d.nakou@uu.nl>
2026-03-25 13:24:02 +0100

    Reapply "added routing for DP map and implemented the tab links for the data access page" and "changed naming: location to map" (#225)
    This reverts commit 9c8b33b93ab90acf59f01b6a33c8280d682d4682.

--- resources/views/public/dataPublication-map.blade.php
+++ resources/views/public/dataPublication-map.blade.php
@@ -1,5 +1,15 @@
 @section('title', 'Datapublications map')
 <x-layout_main>
+    <div class="tab-links-parent">
+        @include('public.components.tab-links', [
+            'categoryName' => 'Data Publications',
+            'routes' => [
+                'List' => route('data-access'),
+                'Map' => route('data-access-map'),
+            ],
+            'routeActive' => route('data-access-map'),
+        ])
+    </div>
 
     <div class="main-content">
         <div class="sub-content-wide flex place-content-center w-full">
