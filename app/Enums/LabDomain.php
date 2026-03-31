1fa277e39925b7db7f3d96d62c8082f60ff6e9aa
d.nakou@uu.nl <d.nakou@uu.nl>
2026-03-31 12:25:10 +0200

    fix bug

--- app/Enums/LabDomain.php
+++ app/Enums/LabDomain.php
@@ -8,7 +8,7 @@ namespace App\Enums;
 enum LabDomain: string
 {
     case ROCK_PHYSICS = 'Rock and melt physics';
-    case ANALOGUE = 'Analogue modelling of geologic processes';
+    case ANALOGUE = 'Analogue modelling of geological processes';
     case MICROSCOPY = 'Microscopy and tomography';
     case PALEO = 'Paleomagnetism';
     case GEO_CHEMISTRY = 'Geochemistry';
