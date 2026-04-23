8d828f99c0c550a00ec5f35cd5b9dc66c9a9c014
Nothbaum <m.nothbaum@uu.nl>
2026-04-23 14:37:53 +0200

    add return type to laboratoryOrganizationUpdateRors()

--- app/Models/LaboratoryOrganizationUpdateGroupRor.php
+++ app/Models/LaboratoryOrganizationUpdateGroupRor.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LaboratoryOrganizationUpdateGroupRor extends Model
 {
     protected $table = 'laboratory_organization_update_group_ror';
 
-    public function laboratoryOrganizationUpdateRors()
+    public function laboratoryOrganizationUpdateRors(): HasMany
     {
         return $this->hasMany(LaboratoryOrganizationUpdateRor::class);
     }
