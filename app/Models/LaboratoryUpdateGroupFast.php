0a64031cc011497777e88c6aa7bc313538882764
Nothbaum <m.nothbaum@uu.nl>
2026-04-23 14:46:06 +0200

    add return type to laboratoryUpdatesFast()

--- app/Models/LaboratoryUpdateGroupFast.php
+++ app/Models/LaboratoryUpdateGroupFast.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LaboratoryUpdateGroupFast extends Model
 {
     protected $table = 'laboratory_update_group_fast';
 
-    public function laboratoryUpdatesFast()
+    public function laboratoryUpdatesFast(): HasMany
     {
         return $this->hasMany(LaboratoryUpdateFast::class);
     }
