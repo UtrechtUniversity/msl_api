c94603be964ac2eaaea95801231ac16309c28b3d
Nothbaum <m.nothbaum@uu.nl>
2026-04-23 14:38:51 +0200

    add return type to laboratoryOrganizationUpdateGroupRor(), laboratoryOrganization()

--- app/Models/LaboratoryUpdateFast.php
+++ app/Models/LaboratoryUpdateFast.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LaboratoryUpdateFast extends Model
 {
@@ -15,12 +16,12 @@ class LaboratoryUpdateFast extends Model
 
     protected $table = 'laboratory_update_fast';
 
-    public function laboratoryUpdateGroupFast()
+    public function laboratoryUpdateGroupFast(): BelongsTo
     {
         return $this->belongsTo(LaboratoryUpdateGroupFast::class);
     }
 
-    public function laboratory()
+    public function laboratory(): BelongsTo
     {
         return $this->belongsTo(Laboratory::class);
     }
