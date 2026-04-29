bdf221b64dfa7aa6e9db1dbd40f07bcbecf853b5
Nothbaum <m.nothbaum@uu.nl>
2026-04-29 14:09:52 +0200

    renaming method source_dataset in resources/views/admin/creates.blade.php to camelCase

--- resources/views/admin/creates.blade.php
+++ resources/views/admin/creates.blade.php
@@ -41,7 +41,7 @@
                                             @break
                                             @endswitch>
                                             <td>{{ $createAction->id }}</td>
-                                            <td>{{ $createAction->source_dataset->source_dataset_identifier->import->importer->name }}
+                                            <td>{{ $createAction->sourceDataset->source_dataset_identifier->import->importer->name }}
                                             </td>
                                             <td>
                                                 @if ($createAction->response_code == '')
