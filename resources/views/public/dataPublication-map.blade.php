2971d76af8ffa4d7ad646a5e0491a6c591cf66ad
d.nakou@uu.nl <d.nakou@uu.nl>
2026-05-04 10:39:57 +0000

    improve position of elements again

--- resources/views/public/dataPublication-map.blade.php
+++ resources/views/public/dataPublication-map.blade.php
@@ -11,11 +11,11 @@
         ])
     </div>
 
-    <div class="main-content">
-        <div class="sub-content-wide flex place-content-center w-full">
-            <div class="drawer lg:drawer-open ">
+    <div class="main-content flex-col h-full">
+        <div class="sub-content-wide flex place-content-center w-full h-full">
+            <div class="drawer lg:drawer-open w-full h-full">
                 <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-                <div class="drawer-content bg-secondary-100 flex relative h-150 ">
+                <div class="drawer-content bg-secondary-100 flex h-full ">
                     {{-- content here --}}
                     <div
                         class="z-30 p-0 w-10 h-52 fixed inset-y-1/2 left-0 -translate-y-26 bg-secondary-200 lg:hidden  opacity-75 hover:opacity-100">
@@ -25,7 +25,7 @@
                             </p>
                         </label>
                     </div>
-                    <div class="w-full min-h-full bg-primary-100 pl-4">
+                    <div class="w-full h-full flex flex-col bg-primary-100 pl-4">
                         {{-- top search div --}}
                         @include('public.components.search-div-list-map')
                         {{-- list view --}}
@@ -35,7 +35,7 @@
                     </div>
 
                 </div>
-                <div class="drawer-side z-40">
+                <div class="drawer-side z-40 h-full">
                     <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
                     {{-- side bar --}}
 
