dfeb607826c99d7ac6173913b26bf373be50677c
Dionysia Nakou <47860263+dionenak@users.noreply.github.com>
2026-05-11 10:41:59 +0200

    MSL-733 Implement base structure for map (#251)

---
+++ resources/views/public/components/datapublication-map/search-div-filters.blade.php
@@ -0,0 +1,19 @@
+<div class="w-80 bg-base-200 flex flex-col place-items-center justify-self-center overflow-auto">
+    <h2 class="py-4">Filters</h1>
+
+        <div class='w-full flex flex-col px-6'>
+
+            <div class="search-bar-container form-field-text p-0 m-0 ">
+                <div class="search-bar-container-icon">
+                    <x-ri-search-line class="search-icon" />
+                </div>
+                <input class="search-bar" type="text" placeholder="Search Filters..." />
+            </div>
+        </div>
+
+        <div class="divide-y w-1/2 flex flex-col place-self-center py-3 divide-primary-700 opacity-50">
+            <div></div>
+            <div></div>
+        </div>
+
+</div>
