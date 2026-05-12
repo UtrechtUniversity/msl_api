fbb7d02c245a94bec551388eb4d9b013ec051306
d.nakou@uu.nl <d.nakou@uu.nl>
2026-05-12 13:27:24 +0200

    make buttons larger

--- resources/views/public/components/datapublication-map/tabs.blade.php
+++ resources/views/public/components/datapublication-map/tabs.blade.php
@@ -1,9 +1,9 @@
 <div id="sidebar-tabs" class=" flex flex-col flex-1 items-center py-5 gap-2 ">
 
-    <div role="tablist" class="tabs tabs-box tabs-md flex flex-row justify-around bg-primary-100 rounded-none w-70">
+    <div role="tablist" class="tabs tabs-box tabs-md flex flex-row justify-around bg-primary-100 rounded-none w-70 gap-1">
         @foreach ($tabs as $tab)
             <a data-tab={{ $tab['name'] }} role="tab"
-                class="tab {{ $tab['default'] ? 'tab-active' : '' }} w-full sm:w-20 hover-interactive !text-primary-900">{{ $tab['name'] }}</a>
+                class="tab {{ $tab['default'] ? 'tab-active' : '' }} flex-1 w-full sm:w-20 hover-interactive !text-primary-900">{{ $tab['name'] }}</a>
         @endforeach
     </div>
 
