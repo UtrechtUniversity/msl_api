ede44cbc8ad844ac9bb4269ba13c4b76491a73dc
d.nakou@uu.nl <d.nakou@uu.nl>
2026-05-13 11:19:19 +0200

    add changes

--- app/Enums/SubDomains/EndpointContext.php
+++ app/Enums/SubDomains/EndpointContext.php
@@ -15,4 +15,17 @@ enum EndpointContext
     case GEO_CHEMISTRY;
     case FIELD_SCALE;
     case ALL;
+
+    public function getDataPublicationSubdomainValue(): ?string
+    {
+        return match ($this) {
+            self::ROCK_PHYSICS => DataPublicationSubDomain::ROCK_PHYSICS->value,
+            self::ANALOGUE => DataPublicationSubDomain::ANALOGUE->value,
+            self::MICROSCOPY => DataPublicationSubDomain::MICROSCOPY->value,
+            self::PALEO => DataPublicationSubDomain::PALEO->value,
+            self::GEO_CHEMISTRY => DataPublicationSubDomain::GEO_CHEMISTRY->value,
+            self::FIELD_SCALE => DataPublicationSubDomain::FIELD_SCALE->value,
+            self::ALL => null
+        };
+    }
 }
