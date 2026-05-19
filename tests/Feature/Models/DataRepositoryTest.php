7a3e77b18c9f401e961b1d44c1ae2f0e20086641
Nothbaum <m.nothbaum@uu.nl>
2026-05-19 20:17:02 +0200

    Relations tests for  Model DataRepository

--- tests/Feature/Models/DataRepositoryTest.php
+++ tests/Feature/Models/DataRepositoryTest.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace Tests\Feature\Models\ImportPipeline;
+namespace Tests\Feature\Models;
 
 use App\Models\DataRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class DataRepositoryImporterRelationTest extends TestCase
+class DataRepositoryTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_data_repository_and_importer_relation(): void
+    public function test_importers_relation(): void
     {
         $repository = DataRepository::create([
             'name' => 'Repository',
@@ -24,7 +24,7 @@ class DataRepositoryImporterRelationTest extends TestCase
             'options' => ['identifierProcessor' => ['type' => 'oai']],
         ]);
 
-        $this->assertCount(1, $repository->importers);
+        $this->assertCount(1, $repository->fresh()->importers);
         $this->assertTrue($repository->importers->contains($importer));
         $this->assertSame($repository->id, $importer->dataRepository->id);
     }
