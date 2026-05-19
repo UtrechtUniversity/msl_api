8bf392d387a8da2657d06872c79cf03d047b44a5
Nothbaum <m.nothbaum@uu.nl>
2026-05-19 20:26:55 +0200

    Relations tests for the Model: Importer

---
+++ tests/Feature/Models/ImporterTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\DataRepository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ImporterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_data_repository_relation(): void
+    {
+        $repository = DataRepository::create([
+            'name' => 'Repository',
+            'ckan_name' => 'repository',
+        ]);
+
+        $importer = $repository->importers()->create([
+            'name' => 'Importer',
+            'description' => 'Importer description',
+            'type' => 'oai',
+            'options' => ['identifierProcessor' => ['type' => 'oai']],
+        ]);
+
+        $this->assertSame($repository->id, $importer->dataRepository->id);
+    }
+
+    public function test_imports_relation(): void
+    {
+        $repository = DataRepository::create([
+            'name' => 'Repository',
+            'ckan_name' => 'repository',
+        ]);
+
+        $importer = $repository->importers()->create([
+            'name' => 'Importer',
+            'description' => 'Importer description',
+            'type' => 'oai',
+            'options' => ['identifierProcessor' => ['type' => 'oai']],
+        ]);
+
+        $import = $importer->imports()->create([]);
+
+        $this->assertCount(1, $importer->fresh()->imports);
+        $this->assertTrue($importer->imports->contains($import));
+        $this->assertSame($importer->id, $import->importer->id);
+    }
+}
