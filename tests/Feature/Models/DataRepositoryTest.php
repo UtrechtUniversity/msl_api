417e955b0eafb01b3a9e2c0f9921d356e43816ab
Nothbaum <m.nothbaum@uu.nl>
2026-05-27 13:51:51 +0200

    added assertInstanceOf check to tests for Model: DataRepository

--- tests/Feature/Models/DataRepositoryTest.php
+++ tests/Feature/Models/DataRepositoryTest.php
@@ -26,6 +26,7 @@ class DataRepositoryTest extends TestCase
 
         $this->assertCount(1, $repository->fresh()->importers);
         $this->assertTrue($repository->importers->contains($importer));
+        $this->assertInstanceOf(DataRepository::class, $importer->dataRepository);
         $this->assertSame($repository->id, $importer->dataRepository->id);
     }
 }
