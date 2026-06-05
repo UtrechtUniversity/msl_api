2841727a5b65443f9d181cda06daa6a24b01868c
d.nakou@uu.nl <d.nakou@uu.nl>
2026-06-05 16:20:13 +0200

    more cleanup

--- resources/ts/types/map.ts
+++ resources/ts/types/map.ts
@@ -1,32 +1,12 @@
-export const EXCLUSIVE = 'exclusive'
-export type Exclusive = typeof EXCLUSIVE
+export const OVERLAPPING = "overlapping";
+export type Overlapping = typeof OVERLAPPING;
 
-export const INCLUSIVE = 'inclusive'
-export type Inclusive = typeof INCLUSIVE
+export const INSIDE = "inside";
+export type Inside = typeof INSIDE;
 
-export type DataPublicationResultSet = Exclusive | Inclusive
-
-
-
-export type DataPublicationResultSetMapping<T> = {
-    [EXCLUSIVE]: T,
-    [INCLUSIVE]: T
-}
-
-
-export const OVERLAPPING = 'overlapping'
-export type Overlapping = typeof OVERLAPPING
-
-export const INSIDE = 'inside'
-export type Inside = typeof INSIDE
-
-export type GeoFeatureResultSet = Overlapping | Inside
-
-
+export type GeoFeatureResultSet = Overlapping | Inside;
 
 export type GeoFeatureResultSetMapping<T> = {
-    [OVERLAPPING]: T,
-    [INSIDE]: T
-}
-
-
+    [OVERLAPPING]: T;
+    [INSIDE]: T;
+};
