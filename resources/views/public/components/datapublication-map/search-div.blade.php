d7e7d669a86010315a7fb8bc39e15a6912bc1749
d.nakou@uu.nl <d.nakou@uu.nl>
2026-06-11 11:51:01 +0200

    move opacity on container level

--- resources/views/public/components/datapublication-map/search-div.blade.php
+++ resources/views/public/components/datapublication-map/search-div.blade.php
@@ -1,12 +1,11 @@
-<div class="search-bar-container form-field-text p-0">
+<div class="search-bar-container form-field-text p-0 opacity-25 pointer-events-none">
     {{-- Search bar --}}
     <div class="search-bar-container-icon">
         <x-ri-search-line class="search-icon" />
 
     </div>
 
-    <form class="w-full h-16" disabled>
-        <input class="search-bar opacity-25 " type="text" id="search" placeholder="Search datapublications.."
-            name="query[]" disabled />
+    <form class="w-full h-16">
+        <input class="search-bar " type="text" id="search" placeholder="Search datapublications.." name="query[]" />
     </form>
 </div>
