9df7e6a54eb48315c9185bd9fe63296a9677f7eb
d.nakou@uu.nl <d.nakou@uu.nl>
2026-06-11 12:10:05 +0200

    text

--- resources/views/public/components/datapublication-map/info.blade.php
+++ resources/views/public/components/datapublication-map/info.blade.php
@@ -2,7 +2,8 @@
      <div class="flex max-[700px]:flex-col items-center place-content-center gap-3">
          {{-- content above map --}}
          <div class="flex justify-center items-center gap-2 min-h-[40px]">
-             <p class="text-info font-bold text-center text-lg"> This is beta version: not all functionalities work
+             <p class="text-info font-bold text-center text-lg"> This is beta version: not all functionalities are
+                 available
                  at
                  the
                  moment.</p>
