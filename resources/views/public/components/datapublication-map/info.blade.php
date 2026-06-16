25a2930280cf9d44763fcd06e05150d3883892ae
Dionysia Nakou <47860263+dionenak@users.noreply.github.com>
2026-06-16 12:49:33 +0200

    MSL-760 Make map ready for production (#276)

--- resources/views/public/components/datapublication-map/info.blade.php
+++ resources/views/public/components/datapublication-map/info.blade.php
@@ -1,7 +1,10 @@
- <div class="flex justify-around pt-6 gap-3">
+ <div class="flex pt-6 gap-3 justify-center">
      <div class="flex max-[700px]:flex-col items-center place-content-center gap-3">
          {{-- content above map --}}
-         <div class="basis-1/2 flex items-center gap-2 min-h-[40px]">
+         <div class="flex justify-center items-center gap-2 min-h-[40px]">
+             <p class="text-info font-bold text-center text-lg"> This is a beta version: not all functionalities are
+                 available
+                 and not all available results are shown.</p>
          </div>
      </div>
      <div>
