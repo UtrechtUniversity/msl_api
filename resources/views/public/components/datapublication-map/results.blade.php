e0a9150152e8226cf9332de695fd7e4ad4cc3cd8
d.nakou@uu.nl <d.nakou@uu.nl>
2026-06-19 13:08:36 +0200

    wip

--- resources/views/public/components/datapublication-map/results.blade.php
+++ resources/views/public/components/datapublication-map/results.blade.php
@@ -1,54 +1,7 @@
       <div id="datapublication-results" class="overflow-y-auto max-h-[720px]">
 
       </div>
-      <div class='mt-auto join divide-x-1'>
+      <div class='mt-auto divide-x-1'>
           <div id='results-pagination' class='flex justify-center border-t divide-x-1 p-2'>
               pagination</div>
       </div>
-
-      {{-- <div class="shrink-0 border-t border-primary-700">
-          <div class="list-view">
-              <div id="pagination-list" class="self-center join p-4 rounded-xl divide-primary-100 divide-x-1">
-
-                  <a href="">
-                      <button class="pagination-button pagination-button-last-left"> <svg class="chevron-icon"
-                              viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                              <path
-                                  d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z">
-                              </path>
-                          </svg> </button>
-                  </a>
-
-                  <a href="http://localhost:8000/data-access?page=1">
-                      <button class="pagination-button pagination-button-active-page">1</button>
-                  </a>
-
-                  <a href="http://localhost:8000/data-access?page=2">
-                      <button class="pagination-button">2</button>
-                  </a>
-
-                  <a href="http://localhost:8000/data-access?page=3">
-                      <button class="pagination-button">3</button>
-                  </a>
-
-                  <a>
-                      <button class="pagination-button btn-disabled !bg-primary-200">...</button>
-                  </a>
-
-                  <a href="http://localhost:8000/data-access?page=223">
-                      <button class="pagination-button">223</button>
-                  </a>
-
-                  <a href="http://localhost:8000/data-access?page=2">
-                      <button class="pagination-button pagination-button-last-right"> <svg class="chevron-icon"
-                              viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                              <path
-                                  d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z">
-                              </path>
-                          </svg> </button>
-                  </a>
-
-              </div>
-              <!-- pagination -->
-          </div>
-      </div> --}}
