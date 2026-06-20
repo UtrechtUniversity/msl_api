538c837a7d06330c95e1d1139d07308301b5dc9a
Samsh001 <l.samshuijzen@uu.nl>
2026-06-20 12:36:40 +0200

    add base ckan driver

---
+++ app/Scout/CkanSearchEngine.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Scout;
+
+use Laravel\Scout\Builder;
+use Laravel\Scout\Engines\Engine;
+
+class CkanSearchEngine extends Engine
+{
+
+    public function update($models)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($models)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function search(Builder $builder)
+    {
+        // TODO: Implement search() method.
+    }
+
+    public function paginate(Builder $builder, $perPage, $page)
+    {
+        // TODO: Implement paginate() method.
+    }
+
+    public function mapIds($results)
+    {
+        // TODO: Implement mapIds() method.
+    }
+
+    public function map(Builder $builder, $results, $model)
+    {
+        // TODO: Implement map() method.
+    }
+
+    public function lazyMap(Builder $builder, $results, $model)
+    {
+        // TODO: Implement lazyMap() method.
+    }
+
+    public function getTotalCount($results)
+    {
+        // TODO: Implement getTotalCount() method.
+    }
+
+    public function flush($model)
+    {
+        // TODO: Implement flush() method.
+    }
+
+    public function createIndex($name, array $options = [])
+    {
+        // TODO: Implement createIndex() method.
+    }
+
+    public function deleteIndex($name)
+    {
+        // TODO: Implement deleteIndex() method.
+    }
+}
