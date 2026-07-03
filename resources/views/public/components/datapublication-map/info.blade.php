c61ad89d9e6b67adced8048449db787f2aa9e8b9
Samsh001 <l.samshuijzen@uu.nl>
2026-07-03 13:38:40 +0200

    change map disclaimer styling and text

--- resources/views/public/components/datapublication-map/info.blade.php
+++ resources/views/public/components/datapublication-map/info.blade.php
@@ -2,9 +2,12 @@
      <div class="flex max-[700px]:flex-col items-center place-content-center gap-3">
          {{-- content above map --}}
          <div class="flex justify-center items-center gap-2 min-h-[40px]">
-             <p class="text-info font-bold text-center text-lg"> This is a beta version: not all functionalities are
-                 available
-                 and not all available results are shown.</p>
+             <div class="bg-info-100 rounded-full mb-3 pl-2 pr-2">
+                 <p class="text-info font-bold text-center text-lg" style="color: var(--color-info-800)">
+                     <x-ri-information-line id="" class="info-icon mx-2" />
+                     This is a beta version: not all functionalities are available. Draw a spatial filter to see results.
+                 </p>
+             </div>
          </div>
      </div>
      <div>
