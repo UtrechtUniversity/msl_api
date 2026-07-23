8b3dc9571c00f8b03e4b66b3de7b2a211561f5aa
d.nakou@uu.nl <d.nakou@uu.nl>
2026-07-23 11:37:12 +0200

    cleanup blade

--- resources/views/public/components/datapublication-map/keyword-tree.blade.php
+++ resources/views/public/components/datapublication-map/keyword-tree.blade.php
@@ -1,9 +1,6 @@
 <div class='overflow-y-auto h-[681px] bg-primary-100'>
     <div class='flex flex-col py-5'>
 
-        @include('public.components.datapublication-map.search-div-filters', [
-            'filterDataPath' => 'public/interpreted.json',
-            'pbDetail' => true,
-        ])
+        @include('public.components.datapublication-map.search-div-filters')
     </div>
 </div>
