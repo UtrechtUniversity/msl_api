f284a313a16a28eb4ce378efc8ad84176fc3311c
d.nakou@uu.nl <d.nakou@uu.nl>
2026-08-31 13:07:32 +0200

    added comment

--- resources/views/public/components/datapublication-map/start-screen-overlay.blade.php
+++ resources/views/public/components/datapublication-map/start-screen-overlay.blade.php
@@ -1,5 +1,7 @@
  <div id="start-screen-overlay"
-     class="absolute inset-0 z-500 bg-primary-200/75 flex flex-col justify-center items-center">
+     class="absolute inset-0
+      {{-- We want this element to be above map-controls (i,e. z-index=1000), but below menu-on-map (i.e. z-index=1100) --}}
+      z-1050 bg-primary-200/75 flex flex-col justify-center items-center">
      <div class="h-1/3 w-1/2 bg-info-300 p-10 ">
 
          <p class='text-xl text-center'>Apply spatial or keyword filter to
