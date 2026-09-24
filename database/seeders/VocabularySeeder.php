ea13b291917686ed0575c4df8603c85f9d024bbd
Sietse Snel <s.t.snel@uu.nl>
2026-09-24 14:43:03 +0200

    Vocabulary seeder: track paths in memory
    When importing vocabulary data, keep track of the
    keyword paths in an array in memory, rather than
    retrieving path data from the database, to improve
    performance of the vocabulary seeder.

--- database/seeders/VocabularySeeder.php
+++ database/seeders/VocabularySeeder.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Str;
 
 class VocabularySeeder extends Seeder
 {
+    private array $keywordPaths = [];
+
     /**
      * Run the database seeds.
      *
@@ -873,11 +875,13 @@ class VocabularySeeder extends Seeder
 
     private function processNode($node, $vocabulary, $parentId = null)
     {
+        $path = $this->buildPath($node->value, $parentId);
+
         $keyword = Keyword::create([
             'parent_id' => $parentId,
             'vocabulary_id' => $vocabulary->id,
             'value' => $node->value,
-            'uri' => '',
+            'uri' => $this->generateURI($path, $vocabulary),
             'external_uri' => $node->external_uri,
             'level' => $node->level,
             'label' => $node->value,
@@ -889,7 +893,7 @@ class VocabularySeeder extends Seeder
             'external_vocab_scheme' => $node->external_vocab_scheme,
         ]);
 
-        $this->generateURI($keyword, $vocabulary);
+        $this->keywordPaths[$keyword->id] = $path;
 
         KeywordSearch::create([
             'keyword_id' => $keyword->id,
@@ -926,11 +930,13 @@ class VocabularySeeder extends Seeder
     private function processNodeOld($node, $vocabulary, $parentId = null, $excludeAbstractMapping = false, $forceExcludeAbstractMapping = false, $excludeSubdomainMapping = false, $forceExcludeSubdomainMapping = false)
     {
 
+        $path = $this->buildPath($node->value, $parentId);
+
         $keyword = Keyword::create([
             'parent_id' => $parentId,
             'vocabulary_id' => $vocabulary->id,
             'value' => $node->value,
-            'uri' => '',
+            'uri' => $this->generateURI($path, $vocabulary),
             'external_uri' => $node->uri,
             'level' => $node->level,
             'label' => $node->value,
@@ -938,7 +944,7 @@ class VocabularySeeder extends Seeder
 
         ]);
 
-        $this->generateURI($keyword, $vocabulary);
+        $this->keywordPaths[$keyword->id] = $path;
 
         KeywordSearch::create([
             'keyword_id' => $keyword->id,
@@ -977,10 +983,21 @@ class VocabularySeeder extends Seeder
         }
     }
 
-    private function generateURI($keyword, $vocabulary)
+    /**
+     * Builds the full path of a keyword from the path of its parent, equal to Keyword::getFullPath()
+     */
+    private function buildPath($value, $parentId)
     {
-        $keyword->uri = $vocabulary->uri.$this->cleanUri($keyword->getFullPath());
-        $keyword->save();
+        if ($parentId === null) {
+            return $value;
+        }
+
+        return $this->keywordPaths[$parentId].'>'.$value;
+    }
+
+    private function generateURI($path, $vocabulary)
+    {
+        return $vocabulary->uri.$this->cleanUri($path);
     }
 
     private function cleanUri($string)
